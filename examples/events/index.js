a39f3867db2be4f1ff5366d0edaecbbea21ba10c
Youri Daamen <me@youz.io>
2015-07-26 22:06:15 +0200

    added property

--- examples/events/index.js
+++ examples/events/index.js
@@ -1,58 +1,61 @@
-require('./style.less')
+require( './style.less' )
 
-var app = require('../../lib/app')
-var Element = require('../../lib/element')
+var app = require( '../../lib/app' )
+var Element = require( '../../lib/element' )
 
-// Element.prototype.inject( require('vjs/lib/methods/setWithPath') )
+Element.prototype.inject(
+  require( '../../lib/property/css' ),
+  require( '../../lib/property/text' )
+)
 
-
-//BUG? path is shared?
-var thing = new Element({
-  $text:'balls',
-  $on:{
-    click:click
+var thing = new Element( {
+  $text: 'balls',
+  $css: {
+    $val: 'grey-bg',
+    $add: ' red-txt '
   },
-  one:{
-    $text:'smalls',
-    $on:{
-      click:click
+  $on: {
+    click: click
+  },
+  one: {
+    $css: 'grey-bg',
+    $text: 'smalls',
+    $on: {
+      click: click
     },
-    two:{
-      $text:'falls',
-      $on:{
-        click:click
+    two: {
+      $css: 'grey-bg',
+      $text: 'falls',
+      $on: {
+        click: click
       },
-      three:{
-        $text:'walls',
-        four:{},
-        $on:{
-          click:click
+      three: {
+        $css: 'grey-bg',
+        $text: 'walls',
+        $on: {
+          click: click
         }
       }
     }
   }
-})
+} )
 
-// thing.one.two.three
-
-// thing.one.two.$remove()
-app.$set({
-  b:new thing.$Constructor({
-    c:new thing.$Constructor({
-      
-    })
-  }),
-  a:new thing.$Constructor()
-})
-
-console.log('------')
-
+app.$set( {
+  b: new thing.$Constructor( {
+    $text: '?!@#234234234234!@#',
+    c: new thing.$Constructor( {
+      flups: new thing.$Constructor( {
+        $text: 'HIERO',
+        $css: 'blue-bg'
+      } )
+    } )
+  } ),
+  a: new thing.$Constructor()
+} )
 
 app.a.one.two.three.$val = 'yuzi'
 
-console.log(app.a)
-
-function click(event, e){
+function click( event, e ) {
   event.$prevent = true
   this.remove()
 }

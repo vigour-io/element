92894521fc657e2d7d099d95ae5c8d489887a512
nikoloza <nika.tomadze@gmail.com>
2015-07-30 18:44:25 +0200

    x and y props

--- examples/property/index.js
+++ examples/property/index.js
@@ -6,26 +6,35 @@ var Element = require( '../../lib/element' )
 Element.prototype.inject(
   require( '../../lib/property/css' ),
   require( '../../lib/property/text' ),
-  require( '../../lib/property/transform' )
+  require( '../../lib/property/transform' ),
+  require( '../../lib/property/size' )
 )
 
 var thing = new Element({
   $on: {
     $click:function(){
+      // console.time('transform, move and dim')
       var x = ~~(Math.random() * 200)
       var y = ~~(Math.random() * 200)
 
-      this.$x.$val = x
       this.$y.$val = y
-      console.log('move that thing!', x, y)
+      this.$x.$val = x
+      // console.info('move that thing!', x, y)
 
-      this.$opacity.$val = Math.random()
-      console.log('make it dim!', x, y)
+      this.$opacity.$val = Math.random() * 2
+      // console.info('make it dim!', this.$opacity.$val)
+
+      this.$width.$val = ~~(Math.random() * 400)
+      this.$height.$val = ~~(Math.random() * 400)
+      // console.info('change sizes!', x, y)
+      // console.timeEnd('transform, move and dim')
     }
   },
-  $x:200,
-  $y:200,
-  $opacity: 1,
+  $x: null,
+  $y: null,
+  $opacity: null,
+  $width: null,
+  $height: null,
   $key:'balls',
   $text: 'balls'
 })

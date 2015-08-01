a8c6ddc75b5964df0e5f37da952ffbb5c3811910
renancarvalho <renansscarvalho@gmail.com>
2015-08-01 15:18:50 +0200

    removed sinon

--- test/events/index.js
+++ test/events/index.js
@@ -1,11 +1,9 @@
-var app 				= require( '../lib/app' )
-var Element 		= require( '../lib/element' )
-var Events 			= require( '../lib/events' )
-var sinon 			= require('sinon')
+var app 				= require( '../../lib/app' )
+var Element 		= require( '../../lib/element' )
 
 Element.prototype.inject(
-  require( '../lib/property/css' ),
-  require( '../lib/property/text' ),
+  require( '../../lib/property/css' ),
+  require( '../../lib/property/text' ),
   require( 'vjs/lib/methods/lookUp' )
 )
 
@@ -21,22 +19,16 @@ var a = new Element( {
 } )
 
 
-var spy = sinon.spy()
-a.on('$click', spy)
-a.on('$grab', spy)
-
 describe( 'Events', function ( done ) {
 	it( 'Should have the click event available inside an element', function ( done ) {
 		a.$emit("$click")
 		expect(a.$val).to.be.equal("clicked")
-		sinon.assert.called(spy)
 		done()
 	})
 
 	it( 'Should have the grab event available inside an element', function ( done ) {
 		a.$emit("$grab")
 		expect(a.$val).to.be.equal("grabed")
-		sinon.assert.called(spy)
 		done()
 	})
 
@@ -50,7 +42,6 @@ describe( 'Events', function ( done ) {
 		})
 		a.$emit("$click")
 		expect(a.$val).to.be.equal("overrided click")
-		sinon.assert.called(spy)
 		done()
 	})
 

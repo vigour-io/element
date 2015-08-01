597336f2124b434baff9f82d0f57e3f661eb1f7e
renancarvalho <renansscarvalho@gmail.com>
2015-08-01 17:05:14 +0200

    Added more tests on property

--- test/properties/index.js
+++ test/properties/index.js
@@ -2,6 +2,8 @@ var Element 		= require( '../../lib/element' )
 
 Element.prototype.inject(
   require( '../../lib/property/css' ),
+  require( '../../lib/property/size' ),
+  require( '../../lib/property/attributes' ),
   require( '../../lib/property/backgroundcolor' ),
   require( '../../lib/property/text' ),
   require( 'vjs/lib/methods/lookUp' )
@@ -9,11 +11,14 @@ Element.prototype.inject(
 
 var a = new Element({
 	$backgroundcolor: 'red',
-	$test: 'simple text', 
-	$css: 'test'
-	
+	$text: 'simple text', 
+	$css: 'test',
+	$width: 100,
+	$height: 100,
+	$attributes: {
+		draggable:true
+	}
 })
-
 
 describe( 'Properties' ,function () {
 	it( 'Should be able to set background-color on a element', function (done) {
@@ -27,8 +32,17 @@ describe( 'Properties' ,function () {
 	})
 
 	it( 'Should be able to set a custom text on a element', function (done) {
-		expect(a.$node).to.be.equal("simple text")
+		expect(a.$node.textContent).to.be.equal("simple text")
 		done()
 	})
 
+	it( 'Shold be able to change the width and the heigh of an element', function (done) {
+		expect(a.$node.style.width).to.be.equal("100px")	
+		expect(a.$node.style.height).to.be.equal("100px")	
+		done()
+	})
+	it( 'Shold be able to any attributes to an element', function (done) {
+		expect(a.$node.draggable).to.be.equal(true)	
+		done()
+	})
 })

add4b09234fa8061d34aa1adc7f8623723368bc9
Youri Daamen <me@youz.io>
2015-08-02 18:05:23 +0200

    added draggable property

--- examples/property/index.js
+++ examples/property/index.js
@@ -7,55 +7,17 @@ Element.prototype.inject(
   require( '../../lib/property/css' ),
   require( '../../lib/property/text' ),
   require( '../../lib/property/transform' ),
+  require( '../../lib/property/draggable' ),
   require( '../../lib/property/size' )
 )
 
 window.thing = thing = new Element({
   $text:'??',
-  $on: {
-    $click: function(){
-
-    },
-    $down: function(event, e){
-      var rect = this.$node.getBoundingClientRect()
-      this.pageX = e.pageX
-      this.pageY = e.pageY
-      this.startX = rect.left
-      this.startY = rect.top
-    },
-    $drag:function(event, e){
-      var x = ~~(this.startX + (e.pageX - this.pageX))
-      var y = ~~(this.startY + (e.pageY - this.pageY))
-      this.$x.$val = x
-      this.$y.$val = y
-      this.$text.$val = x + ' , ' + y
-      this.$node.style.opacity = 1 - x / window.innerWidth
-    }
-  },
-  $x: 1,
-  $y: 1
-  // span: {
-  //   $node: 'span',
-  //   $text: 'Drag Me!'
-  // }
+  $draggable:true
 })
 
-// //if id === 3
-// thing.$on.setKey('$down',{
-//   3:function( event, e ) {
-//     var _this = this
-//     app.on('$move',function( event, e ){
-//       _this.$emit( key, event, e )
-//     },3)
-
-//     app.on('$up',function(){
-//       app.off('$move',3)
-//       app.off('$up',3)
-//     },3)
-//   }
-// })
-
 app.set( {
-  a: new thing.$Constructor()
+  a: new thing.$Constructor({
+    $text:'>>>'
+  })
 } )
-

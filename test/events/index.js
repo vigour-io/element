95bde8f7ab22b3e07cd5afa6df30646d5c18497e
Youri Daamen <me@youz.io>
2015-08-02 15:14:02 +0200

    modulized events, added specific up event on up

--- test/events/index.js
+++ test/events/index.js
@@ -6,41 +6,63 @@ Element.prototype.inject(
   require( 'vjs/lib/methods/lookUp' )
 )
 
+var click = 0
+var grab = 0
 var a = new Element( {
 	$on:{
 		$click:function (argument) {
-			this.$val = "clicked"
+			this.$val = ++click
 		},
 		$grab:function (argument) {
-			this.$val = "grabbed"
+			this.$val = ++grab
 		}
 	}
 } )
 
+var b = new a.$Constructor({
+	$val:'own value'
+})
+
 describe( 'Events', function ( done ) {
 	it( 'Click event available inside an element', function ( done ) {
-		a.$emit("$click")
-		expect(a.$val).to.be.equal("clicked")
+		
+		a.$emit('$click')
+		expect(a.$val).to.be.equal(1)
+		expect(b.$val).to.be.equal('own value')
+		
+		b.$emit('$click')
+		expect(a.$val).to.be.equal(1)
+		expect(b.$val).to.be.equal(2)
+
 		done()
 	})
 
 	it( 'Grab event available inside an element', function ( done ) {
-		a.$emit("$grab")
-		expect(a.$val).to.be.equal("grabbed")
+		a.$emit('$grab')
+		expect(a.$val).to.be.equal(1)
+		expect(b.$val).to.be.equal(2)
 		done()
 	})
 
 	it( 'Override an event', function( done ) { 
-		a.set({
-			$on:{
-				$click:function () {
-					this.$val = "overridden click"
-				}
-			}
-		})
-		a.$emit("$click")
-		expect(a.$val).to.be.equal("overridden click")
+		a.$on.$click.$val = function () {
+			this.$val = 'overridden click'
+		}
+		a.$emit('$click')
+		expect(a.$val).to.be.equal('overridden click')
 		done()
 	})
 
+	it( 'Remove an event', function( done ) { 
+		
+		a.$on.$click.$val = function () {
+			this.$val = 'not removed'
+		}
+
+		a.$on.$click.remove()
+		a.$emit('$click')
+		expect(a.$val).to.be.equal('overridden click')
+		done()
+	})
+	
 })

06dda4f43dc71a5c88da69d64b85b03c57c66bd4
Youri Daamen <me@youz.io>
2015-08-03 13:28:17 +0200

    debugging operator on props

--- examples/cases/index.js
+++ examples/cases/index.js
@@ -9,11 +9,49 @@ Element.prototype.inject(
   require( 'vjs/lib/methods/lookUp' )
 )
 
+
+var Observable = require('vjs/lib/observable')
+// Observable.prototype.inject(require('vjs/lib/operator/add'))
+
+a = new Observable({
+  $key:'a',
+  $val:1
+})
+
+b = new Observable({
+  $key:'b',
+  $val:1
+})
+
+c = new Observable({
+  $val: a,
+  $add: b
+})
+
+a.$val = 'x'
+b.$val = 'y'
+
 var thing = new Element( {
-  // $key:'balls',
-  $text:'ha'
+  $text:{
+	  $val: 'a',
+	  $add: 'b'
+  }
 } )
 
+var Event = require( 'vjs/lib/event' )
+var bla = new Event(app)
+
 app.set({
-  a:thing
+  b:new Element({
+	  $text:{
+		  $val: 'a',
+		  $add: 'b'
+	  }
+  })
 })
+
+
+
+console.log(c.$val)
+
+// console.log(app.a.$text.$val)

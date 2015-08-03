7fe4a5fc434d6a92ba2cdc17fa967a67093b7061
nikoloza <nika.tomadze@gmail.com>
2015-08-03 03:37:07 +0200

    css drag experiment

--- examples/drag-animations/index.js
+++ examples/drag-animations/index.js
@@ -1,33 +1,39 @@
-require( './style.less' )
+ require( './style.less' )
 
 var app = require( '../../lib/app' )
 var Element = require( '../../lib/element' )
 
 Element.prototype.inject(
   require( '../../lib/property/css' ),
+  require( '../../lib/property/transform' ),
+  require( '../../lib/property/draggable' ),
   require( '../../lib/property/text' )
 )
 
+var dragPoint = 0
 var thing = new Element( {
   $text: 'Hello',
   $css: 'hello',
-  $on: {
-    $down: function(event, e){
-      var rect = this.$node.getBoundingClientRect()
-      this.startX = rect.left
-      this.startY = rect.top
-    },
+  // $on: {
+  //   $drag: function(event, e) {
+  //     if (dragPoint > e.x)
+  //       this.$css.$val = 'hello toRight'
+  //     else
+  //       this.$css.$val = 'hello toLeft'
 
-    $grab: function(event, e){
-      this.$x.$val = e.x - 100 //- this.startX
-      this.$y.$val = e.y - 100 //- this.startY
-    }
-  },
+  //     // this.$transformOrigin.$val = e.layerX + ' ' + e.layerY
+
+  //     dragPoint = e.x
+  //   }
+  // }
 });
 
 
 app.set( {
-  a: new thing.$Constructor({
-
-  })
+  hello: new thing.$Constructor(),
+  $draggable: {
+    bind: function(){
+      return this.hello
+    }
+  }
 })

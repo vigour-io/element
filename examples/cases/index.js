32105868c98417b6ec3cc012640906b585459176
Youri Daamen <me@youz.io>
2015-08-03 20:12:21 +0200

    cases example

--- examples/cases/index.js
+++ examples/cases/index.js
@@ -5,25 +5,25 @@ var Element = require( '../../lib/element' )
 
 Element.prototype.inject(
   require( '../../lib/property/css' ),
-  require( '../../lib/property/text' ),
-  require('../../lib/cases')
+  require( '../../lib/property/text' ) 
 )
+// console.clear()
 
-console.clear()
-cases.set({
-	$connected:true,
-	$isreceiver:true
+
+// var cases = require('../../lib/cases')
+
+// cases.set({
+// 	$connected:false,
+// 	$isreceiver:true
+// })
+
+var b = new Element({
+  $text:{
+	  $val: 'not desk!!',
+	  $add:'bawler'
+  }
 })
 
 app.set({
-  b:new Element({
-	  $text:{
-		  $val: 'not desk!!',
-		  $desktop:{
-		  	$val:'smur!!',
-		  	// $case:true//or false
-		  	// $add:'desktop!!' //an operator which is an instance of observable
-		  }
-	  }
-  })
+  b:b
 })

345d9b8ae0fefa89fe1dcf12efb2454571613d58
Youri Daamen <me@youz.io>
2015-08-07 16:17:15 +0200

    added app.$width and app.$height, cases concept done

--- examples/cases/index.js
+++ examples/cases/index.js
@@ -2,49 +2,39 @@ require( './style.less' )
 
 var app = require( '../../lib/app' )
 var Element = require( '../../lib/element' )
-// var Operator = require('vjs/lib/operator')
 Element.prototype.inject(
   require( '../../lib/property/css' ),
   require( '../../lib/property/text' ),
   require( '../../lib/property/backgroundImage' )
 )
-// console.clear()
 
+var cases = require('../../lib/cases')
 
-// var cases = require('../../lib/cases')
-
-// cases.set({
-// 	$connected:false,
-// 	$isreceiver:true
-// })
-
-var a = new Element({
-	$css:'funlife',
-  $text:{
-	  $val: 'A & ',
-	  $add:'bawler'
-  },
-  $on:{
-  	$click:function(){
-  		console.error('HEY!!')
-  	}
-  },
-  $backgroundImage:'http://www.cdc.gov/importation/images/dog2.jpg'
+cases.set({
+	$bigscreen:{
+		$val:app.$width,
+		$transform:function(val,event){
+			return val > 400
+		}
+	},
+	$connected:false
 })
 
 app.set({
-	a:new a.$Constructor()
-})
-
-app.set({
-	b:new Element({
+	a:new Element({
+		$css:'funlife',
 	  $text:{
-		  $val: 'B & ',
-		  $add:'bawler'
+		  $val: app.$width,
+		  $phone:{
+		  	$val:'bawler',
+		  	$add:'!!!'
+		  },
+		  $desktop:{
+		  	$val:'desktop, but not connected',
+		  	$connected:'I am connected!!',
+		  	$bigscreen:'BIG!'
+		  }
 	  },
-	  c:new a.$Constructor({
-	  	d:new a.$Constructor()
-	  })
+	  $backgroundImage:'http://www.cdc.gov/importation/images/dog2.jpg'
 	})
 })
-

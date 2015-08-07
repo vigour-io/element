38bbd1808b686780d6650151ab776175073ce8a4
nikoloza <nika.tomadze@gmail.com>
2015-08-07 14:25:59 +0200

    tests have been fixed for transforms

--- test/properties/transforms.js
+++ test/properties/transforms.js
@@ -7,7 +7,8 @@ Element.prototype.inject(
   require('../../lib/property/css'),
   require('../../lib/property/size'),
   require('../../lib/property/attributes'),
-  require('../../lib/property/transform'),//.extended(),
+  require('../../lib/property/transform'),
+  require('../../lib/property/opacity'),
   require('../../lib/property/backgroundcolor'),
   require('../../lib/property/backgroundImage'),
   require('../../lib/property/text')
@@ -37,7 +38,7 @@ describe('creating new property', function(){
   })
 })
 
-describe('$x, $y, $z', function() {
+describe('$x, $y', function() {
   it('should set translate3d property for $x', function () {
     var value = 440
 
@@ -58,16 +59,6 @@ describe('$x, $y, $z', function() {
     expect(elem.$node.style.transform).to.equal('translate3d(0px, ' + value + 'px, 0px)')
   })
 
-  it('should set translate3d property for $z', function () {
-    var value = 15
-
-    var elem = new Element({
-      $z: value
-    })
-
-    expect(elem.$node.style.transform).to.equal('translate3d(0px, 0px, ' + value + 'px)')
-  })
-
   describe('-> together', function() {
     it('should set translate3d property for $x and $y', function () {
       var valueX = 440
@@ -80,106 +71,13 @@ describe('$x, $y, $z', function() {
 
       expect(elem.$node.style.transform).to.equal('translate3d(' + valueX + 'px, ' + valueY + 'px, 0px)')
     })
-
-    it('should set translate3d property for $x and $z', function () {
-      var valueX = 440
-      var valueZ = 12
-
-      var elem = new Element({
-        $x: valueX,
-        $z: valueZ
-      })
-
-      expect(elem.$node.style.transform).to.equal('translate3d(' + valueX + 'px, 0px, ' + valueZ + 'px)')
-    })
-
-    it('should set translate3d property for $y and $z', function () {
-      var valueY = 221
-      var valueZ = 12
-
-      var elem = new Element({
-        $y: valueY,
-        $z: valueZ
-      })
-
-      expect(elem.$node.style.transform).to.equal('translate3d(0px, ' + valueY + 'px, ' + valueZ + 'px)')
-    })
-
-    it('should set translate3d property for $x, $y and $z', function () {
-      var valueX = 120
-      var valueY = 221
-      var valueZ = 19
-
-      var elem = new Element({
-        $x: valueX,
-        $y: valueY,
-        $z: valueZ
-      })
-
-      expect(elem.$node.style.transform).to.equal('translate3d(' + valueX + 'px, ' + valueY + 'px, ' + valueZ + 'px)')
-    })
   })
 })
 
-describe('$translate', function() {
-  it('should set translate property on the element', function () {
-    var value = '20px, 30px'
-
-    var elem = new Element({
-      $translate: value
-    })
-
-    expect(elem.$node.style.transform).to.equal('translate(' + value + ')')
-  })
-})
-
-describe('$translate3d', function() {
-  it('should set translate3d property on the element', function () {
-    var value = '20px, 30px, 0px'
-
-    var elem = new Element({
-      $translate3d: value
-    })
-
-    expect(elem.$node.style.transform).to.equal('translate3d(' + value + ')')
-  })
-})
-
-describe('$skew, $skewX and $skewY', function() {
-  it('should set skew properties on the element', function () {
-    var value = '30deg, 20deg'
-
-    var elem = new Element({
-      $skew: value
-    })
-
-    expect(elem.$node.style.transform).to.equal('skew(' + value + ')')
-  })
-
-  it('should set skew propertiy for only X', function () {
-    var value = 11
-
-    var elem = new Element({
-      $skewX: value
-    })
-
-    expect(elem.$node.style.transform).to.equal('skew(' + value + 'deg, 0deg)')
-  })
-
-  it('should set skew property for only Y', function () {
-    var value = 44
-
-    var elem = new Element({
-      $skewY: value
-    })
-
-    expect(elem.$node.style.transform).to.equal('skew(0deg, ' + value + 'deg)')
-  })
-})
 
 describe('$scale', function() {
   it('should set scale property on the element', function () {
-    var value = '1.1, 1.4'
+    var value = 1.2
 
     var elem = new Element({
       $scale: value
@@ -187,95 +85,37 @@ describe('$scale', function() {
 
     expect(elem.$node.style.transform).to.equal('scale(' + value + ')')
   })
-
-  it('should set scale propertiy for only X', function () {
-    var value = 1.5
-
-    var elem = new Element({
-      $scaleX: value
-    })
-
-    expect(elem.$node.style.transform).to.equal('scale(' + value + ', 0)')
-  })
-
-  it('should set scale property for only Y', function () {
-    var value = 1.8
-
-    var elem = new Element({
-      $scaleY: value
-    })
-
-    expect(elem.$node.style.transform).to.equal('scale(0, ' + value + ')')
-  })
 })
 
 describe('$rotate', function() {
   it('should set rotate property on the element', function () {
-    var value = '180deg'
+    var value = 180
 
     var elem = new Element({
       $rotate: value
     })
 
-    expect(elem.$node.style.transform).to.equal('rotate(' + value + ')')
+    expect(elem.$node.style.transform).to.equal('rotate(' + value + 'deg)')
   })
 })
 
 describe('-> together', function() {
-  it('should set x, y, rotate and scaleY properties together', function () {
+  it('should set x, y, rotate and scale properties together', function () {
     var x = 150,
         y = 440,
-        rotate = '15deg',
-        scaleY = 2.4
+        rotate = 15,
+        scale = 2.4
 
     var elem = new Element({
       $x: x,
       $y: y,
       $rotate: rotate,
-      $scaleY: scaleY
-    })
-
-    expect(elem.$node.style.transform).to.equal(
-      'rotate(' + rotate + ') ' +
-      'translate3d(' + x + 'px, ' + y + 'px, 0px) ' +
-      'scale(0, ' + scaleY + ')'
-    )
-  })
-
-  it('should set x, z, skewX and scaleX properties together', function () {
-    var x = 330,
-        z = 4,
-        skewX = 15,
-        scaleX = 1.7
-
-    var elem = new Element({
-      $x: x,
-      $z: z,
-      $skewX: skewX,
-      $scaleX: scaleX
-    })
-
-    expect(elem.$node.style.transform).to.equal(
-      'translate3d(' + x + 'px, 0px, '+ z + 'px) ' +
-      'skew(' + skewX + 'deg, 0deg) ' +
-      'scale(' + scaleX + ', 0)'
-    )
-  })
-
-  it('should set translate, rotate and scale properties together', function () {
-    var translate = '330px, 40px',
-        rotate = '15deg',
-        scale = '0.8, 1.7'
-
-    var elem = new Element({
-      $translate: translate,
-      $rotate: rotate,
       $scale: scale
     })
 
     expect(elem.$node.style.transform).to.equal(
-      'rotate(' + rotate + ') ' +
-      'translate(' + translate + ') ' +
+      'translate3d(' + x + 'px, ' + y + 'px, 0px) ' +
+      'rotate(' + rotate + 'deg) ' +
       'scale(' + scale + ')'
     )
   })
@@ -297,8 +137,7 @@ describe('$opacity', function() {
 describe('adding transforms', function () {
   var valueX = 100
   var valueY = 270
-  var skewX = 15
-  var scaleX = 1.7
+  var scale = 1.7
 
   var elem = new Element({
     $x: valueX
@@ -314,14 +153,12 @@ describe('adding transforms', function () {
     expect(elem.$node.style.transform).to.equal('translate3d(' + valueX + 'px, ' + valueY + 'px, 0px)')
 
     elem.set({
-      $skewX: skewX,
-      $scaleX: scaleX
+      $scale: scale
     })
 
     expect(elem.$node.style.transform).to.equal(
       'translate3d(' + valueX + 'px, ' + valueY + 'px, 0px) ' +
-      'skew(' + skewX + 'deg, 0deg) ' +
-      'scale(' + scaleX + ', 0)'
+      'scale(' + scale + ')'
     )
   })
 })
@@ -329,9 +166,6 @@ describe('adding transforms', function () {
 
 describe('removing transforms', function () {
   var valueX = 100
-  var valueY = 270
-  var opacity = 1.6
-  var skewY = '30deg'
 
   var elem = new Element({
     $x: valueX
@@ -342,6 +176,6 @@ describe('removing transforms', function () {
 
     elem.$x.remove()
 
-    expect(elem.$node.style.transform).to.equal('translate3d(0px, 0px, 0px)')
+    expect(elem.$node.style.transform).to.equal('')
   })
 })

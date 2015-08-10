b79c6180035c34e8042a704e417eee81a663c59c
renancarvalho <renansscarvalho@gmail.com>
2015-08-10 18:51:35 +0200

    More tests scenarios for attributes

--- test/properties/attributes.js
+++ test/properties/attributes.js
@@ -15,8 +15,8 @@ describe( '--> Attributes' , function () {
 	})
 
 	afterEach(function () {
-		a.$attributes.custom.set({
-			$val :true
+		a.$attributes.set({
+			custom: true
 		})
 	})
 
@@ -34,24 +34,46 @@ describe( '--> Attributes' , function () {
 	describe( 'inheriting elements' , function () {
 		var b = new a.$Constructor()
 
-		it( 'b should have the same attributes of a' , function () {
+		it( '\'b\' should have the same attributes of \'a\'' , function () {
 			expect(b.$node.getAttribute('custom')).to.be.equals("true")
 		})
 
-		it( 'a changes should change b' , function () {
+		it( '\'a\' changes should change \'b\' too' , function () {
 			a.$attributes.custom.set({
 				$val :"newValue"
 			})
-			// Should update as well, no ?
+			// Should update as well, no youri?
+			// the weird thing is that b.$attributes.custom.$val === "newValue"
 			expect(b.$node.getAttribute('custom')).to.be.equals("newValue")
 		})
 
-		it( 'b changes should not change a' , function () {
-			b.$attributes.custom.set({
-				$val :"bValue"
+		it( '\'b\' changes should not change \'a\'' , function () {
+			b.$attributes.set({
+				custom :"bValue"
 			})
 			expect(a.$node.getAttribute('custom')).to.be.equals("true")
 		})
+
+		it( 'remove custom attributes from \'b\'' , function () {
+			b.$attributes.custom.remove()
+			expect(b.$node.getAttribute('custom')).to.be.null
+		})
+
+		it( 'attributes should be defined' , function () {
+			expect(b.$attributes).to.be.ok
+		})
+
+
+		it( 'attributes should be defined' , function () {
+			expect(b.$attributes).to.be.ok
+		})
+
+		it( 'remove \'a\' custom attribute should remove \'b\' too' , function () {
+			a.$attributes.custom.remove()
+			expect(b.$attributes.custom).to.be.null
+		})
+
+
 	})
 
 })

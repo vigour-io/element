d0cb52f3d4e9b6e7d2fb761ff02c7379f8a01d4f
nikoloza <nika.tomadze@gmail.com>
2015-08-12 15:36:06 +0200

    redefining some "less" staff

--- examples/animation/index.js
+++ examples/animation/index.js
@@ -16,21 +16,6 @@ var thing = window.thing = new Element( {
   $css: 'hello test',
   $on: {
     $click: function () {
-      var that = this;
-
-      function anim () {
-        var x = this.$x && this.$x.val || 0
-        var y = this.$y && this.$y.val || 0
-
-        that.set({
-          $x: x + 1,
-          $y: y + 1
-        })
-
-        requestAnimationFrame(anim)
-      }
-
-      asd = requestAnimationFrame(anim)
 
     }
   }

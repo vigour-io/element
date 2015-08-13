faf4851928480fbbeef011c8e3511483d61d4871
renancarvalho <renansscarvalho@gmail.com>
2015-08-13 17:19:07 +0200

    Added tests for input required logic

--- test/input/index.js
+++ test/input/index.js
@@ -29,7 +29,7 @@ describe( '--> Using the input component' , function () {
 			}
 		}
 	})
-	app.set({renan:a})
+	app.set({inputField:a})
 
 	it( 'creating a simple input type text' , function () {
 		expect(a.$node.type).to.be.equals('text')
@@ -50,3 +50,52 @@ describe( '--> Using the input component' , function () {
 		expect(txt).to.be.equals("yep")
 	})
 })
+
+
+describe( '--> Input with required true' , function(){
+
+  var a = new Input({
+    $attributes:{
+      type:"text"
+    },
+    $required:{
+      required: true,
+      message: "This is a custom message for required fields",
+      defaultStyle : true
+    },
+    $on:{
+      $validation:function( event, meta ) {
+        txt = ""
+        txt = meta
+      }
+    }
+  })
+  app.set({inputRequired:a})
+
+  it( 'should use the default error behaviour when the the input is empty' ,function () {
+    a.$node.value = ""
+    fireEvent( a.$node, 'blur')
+    expect(a.$node.style.borderColor).to.be.equal('red')
+  })
+
+  it( 'should be fine if the input is not empty' ,function () {
+    a.$node.value = "some text"
+    fireEvent( a.$node, 'blur')
+    expect(a.$node.style.borderColor).to.not.be.equal('red')
+  })
+
+  it( 'should use the custom message' ,function () {
+    a.$node.value = "some text"
+    fireEvent( a.$node, 'blur')
+    expect(txt).to.be.equal('This is a custom message for required fields')
+  })
+
+})
+
+
+
+
+
+
+
+

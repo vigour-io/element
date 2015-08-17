ca1043d0460034322bb3a4c593e9ba9254ba473e
nikoloza <nika.tomadze@gmail.com>
2015-08-17 14:50:21 +0200

    css animation basic settings with js callback

--- examples/js-animation/index.js
+++ examples/js-animation/index.js
@@ -1,4 +1,6 @@
- require( './style.less' )
+console.clear()
+
+require( './style.less' )
 
 var app = require( '../../lib/app' )
 var Element = require( '../../lib/element' )
@@ -6,6 +8,8 @@ var Element = require( '../../lib/element' )
 Element.prototype.inject(
   require( '../../lib/property/css' ),
   require( '../../lib/property/transform' ),
+  require( '../../lib/property/opacity' ),
+  require( '../../lib/property/transition' ),
   require( '../../lib/property/animate' )
 )
 
@@ -18,17 +22,17 @@ var thing = window.thing = new Element( {
 app.set( {
   hello: new thing.$Constructor(),
   $on:{
-    $click:function(event, e){
+    $click: function(event, e){
       this.hello.set({
         $animate:{
           $x: e.x,
           $y: e.y,
-          $opacity: 0.2,
-          $easing: 'easeInOut',
-          $time: 600,
-          $delay: 500,
-          $callback: function () {
-            console.log('done', new Date())
+          $opacity: Math.random(),
+          $easing: 'easeIn',
+          $time: Math.random() * 1000,
+          $delay: 0,
+          $callback: function (event) {
+            console.log('>>> callback fired!', event)
           }
         }
       })

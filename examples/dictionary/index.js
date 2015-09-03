e667e04dc9f7b30e941ce6537a747a21546d326c
Youri Daamen <me@youz.io>
2015-09-03 17:30:01 +0200

    dictionary

--- examples/dictionary/index.js
+++ examples/dictionary/index.js
@@ -3,15 +3,19 @@ require('./style.less')
 var app = require('../../lib/app')
 var dictionary = window.dictionary = require('../../lib/dictionary')
 var Element = require('../../lib/element')
+var Observable = require('vjs/lib/observable')
 
 dictionary.set( require('./en.json') )
-
-Element.prototype.inject( require('../../lib/dictionary/text') )
+Observable.prototype.inject( require('../../lib/dictionary/text') )
+Element.prototype.inject( require('../../lib/property/text') )
 
 app.set({
   test:{
-    $text:'message.success'
+    $text:{
+    	$dictionary:'message.success',
+    	$add:{
+    		$dictionary:'message.error'
+    	}
+    }
   }
 })
-
-// app.test.$text.$val = 'message.error'

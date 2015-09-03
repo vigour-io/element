0b4d0a1e095f7380c6f230e419a4614976cdf245
Youri Daamen <me@youz.io>
2015-09-03 19:22:06 +0200

    dictionary

--- examples/dictionary/index.js
+++ examples/dictionary/index.js
@@ -4,18 +4,32 @@ var app = require('../../lib/app')
 var dictionary = window.dictionary = require('../../lib/dictionary')
 var Element = require('../../lib/element')
 var Observable = require('vjs/lib/observable')
+var en = require('./en.json')
+var nl = require('./nl.json')
+var toggle = true
+dictionary.set( en )
 
-dictionary.set( require('./en.json') )
-Observable.prototype.inject( require('../../lib/dictionary/text') )
 Element.prototype.inject( require('../../lib/property/text') )
+Observable.prototype.inject( require('../../lib/dictionary/inject') )
 
 app.set({
   test:{
     $text:{
     	$dictionary:'message.success',
-    	$add:{
-    		$dictionary:'message.error'
-    	}
+    	$transform:[
+    		function(val){
+    			return 'balls'
+    		},
+    		function(val){
+    			return 'smalls'
+    		}
+    	],
+    	$add:['1','2','3','4','5']
     }
   }
 })
+
+setInterval(function(){
+	dictionary.set( toggle ? nl : en )
+	toggle = !toggle
+},1000)

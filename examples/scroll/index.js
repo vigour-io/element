0a84ae23ebc42306fd92ae5f270c15060c19461c
Youri Daamen <me@youz.io>
2015-09-05 10:23:16 +0200

    custom scroll testing

--- examples/scroll/index.js
+++ examples/scroll/index.js
@@ -11,31 +11,62 @@ Element.prototype.inject(
 )
 
 var thing = new Element({
-  $height: 700
+  $height: 300
 })
 
-var holder = new Element({})
+var holder = new Element({
+  $scrollTop: {
+    $val: 0,
+    $animation: {
+      $duration: 16
+    }
+  },
+  $on:{
+    touchstart:function( event, e ){
+      e.preventDefault()
+      this._prev = e.y
+      this.$scrollTop.setKey( '$dragging', true )
+    },
+    touchmove:function( event, e ){
+      e.preventDefault()
+      var d = ( e.y - this._prev )
+      this.$scrollTop.$val -= d
+      this._delta = d
+      this._prev = e.y
+    },
+    touchend:function( event, e ){
+      var $scrollTop = this.$scrollTop
+      var d = this._delta
+      console.log(d)
+      $scrollTop.set({
+        $dragging:false,
+        $val:$scrollTop.$val - d * Math.abs(d/2),
+        $animation:{
+          $duration:Math.abs(d/2)
+        }
+      })
+    }
+  }
+})
+
 var chooser = new Element({})
 var colors = ['yellow', 'orange', 'blue', 'lilac', 'pink']
 
 for(var i = 0 ; i < 5; i=i+1) {
   var n = new thing.$Constructor()
-
   var m = new Element({
     $define: {
-      i: i*700
+      i: i*300
     },
     $on: {
       $click: function (ev, event) {
-        app.setKey('$scrollTop', this.i)
+        app.holder.setKey('$scrollTop', this.i)
       }
     }
   })
-
   holder.setKey(colors[i], n)
   chooser.setKey(colors[i], m)
 }
-
 app.set({
   holder: holder,
   chooser: chooser,
@@ -47,10 +78,14 @@ app.set({
   }
 })
 
+document.body.addEventListener('touchstart',function(e){
+  e.preventDefault();
+},false)
+
 app.set({
   arrow:{
     $y:{
-      $val:app.$scrollTop,
+      $val:app.holder.$scrollTop,
       $transform:function( val ){
         var b = document.body
         return val * b.offsetHeight/b.scrollHeight

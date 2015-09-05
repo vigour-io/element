2b691b4e3da7ab5f8b388ec035b34ad1c2b03532
Youri Daamen <me@youz.io>
2015-09-05 18:53:10 +0200

    fix in events, extended scroll test

--- examples/scroll/index.js
+++ examples/scroll/index.js
@@ -10,24 +10,44 @@ Element.prototype.inject(
   require( '../../lib/property/transform' )
 )
 
+var Bla = new Element({
+  $width: 200,
+  $height:200,
+}).$Constructor
+
 var thing = new Element({
-  $height: 300
+  $height: 300,
+  scroller:new Element({
+    $scrollLeft:true,
+    one:new Bla(),
+    two:new Bla(),
+    three:new Bla(),
+    four:new Bla(),
+    five:new Bla(),
+    six:new Bla()
+  })
 })
 
 var holder = new Element({
-  $scrollTop:{
-    $val:0,
-    $animation:{
-      $duration:24
-    }
-  }
+  $scrollTop:true
+  // $scrollTop:{
+  //   $val:0,
+  //   $animation:{
+  //     $duration:36
+  //   },
+  //   // $on:{
+  //   //   $change:function(){
+  //   //     console.log('unified scroll!!',this.$val)
+  //   //   }
+  //   // }
+  // }
 })
 
 var chooser = new Element({})
 var colors = ['yellow', 'orange', 'blue', 'lilac', 'pink']
 
 for(var i = 0 ; i < 5; i=i+1) {
-  var n = new thing.$Constructor()
+  var n = new thing.$Constructor({$scrollLeft:true})
   var m = new Element({
     $define: {
       i: i*300
@@ -41,25 +61,13 @@ for(var i = 0 ; i < 5; i=i+1) {
   holder.setKey(colors[i], n)
   chooser.setKey(colors[i], m)
 }
+
 app.set({
   holder: holder,
   chooser: chooser,
-  $scrollTop: {
-    $val: 0,
-    $animation: {
-      $duration: 16
-    }
-  }
-})
-
-document.body.addEventListener('touchstart',function(e){
-  e.preventDefault()
-},false)
-
-app.set({
   arrow:{
     $y:{
-      $val:app.holder.$scrollTop,
+      $val:holder.$scrollTop,
       $transform:function( val ){
         var b = document.body
         return val * b.offsetHeight/b.scrollHeight

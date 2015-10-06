9d55ee4398f9fd3bff564b53407c37aeaf0302ad
Jim de Beer <jim@vigour.io>
2015-10-06 12:15:03 +0200

    remove logs

--- examples/animnewoperator/index.js
+++ examples/animnewoperator/index.js
@@ -17,22 +17,45 @@ var Bla = new Element({
   key: 'bla',
 }).Constructor
 
+var Observable = require('vjs/lib/observable')
+
 var thing = new Element({
+  properties: {
+    bla: Observable
+  },
   css:'thing',
-  x:{ val: 20, $animation: 20 },
-  text: 'a'
+  x: {
+    val: 20,
+    $animation: 2
+    // on: {
+    //   data: function() {
+    //     console.error('ok data update', this.path)
+    //   }
+    // }
+  },
+  text: {
+    on: {
+      parent: function (parent, event) {
+        //does not work with useval?
+        console.log('parent anyone? does not seem to work...', parent.key)
+        this.set( parent.key, event )
+      }
+    }
+  }
 })
 
 app.set({
   a: new thing.Constructor(),
   b: new thing.Constructor(),
-  c: new thing.Constructor()
+  c: new thing.Constructor(),
+  d: new thing.Constructor()
 })
 
-app.set({
-  // text: app.a.x.val
-})
+console.clear()
 
+console.log(thing._instances)
+
+// thing.clearContext()
 thing.x.val = 300
 
 

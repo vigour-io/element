b3c4a2969ce7c20940b3a2e3baea8276a78515e6
Youri Daamen <me@youz.io>
2015-10-15 01:09:52 +0200

    code dablings

--- examples/random-example/index.js
+++ examples/random-example/index.js
@@ -9,3 +9,41 @@ app.set({
     }
   }
 })
+
+// var Slide = new Element({
+//   ChildConstructor:false,
+//   properties:{
+//     title:TextElement,
+//     subtitle:TextElement,
+//     img:ImgElement
+//   }
+// }).Constructor
+
+// var Carousel = new Element({
+//   ChildConstructor:Slide
+// }).ChildConstructor
+
+// app.set({
+//   carousel:new Carousel({
+//     //episodes bijv
+//     1:{
+//       title:'nerdjes unite',
+//       subtitle:'fly as a dove',
+//       img:'be cool'
+//     },
+//     2:{
+//       title:'nerdjes unite',
+//       subtitle:'fly as a dove',
+//       img:'be cool'
+//     },
+//     3:{
+//       title:'nerdjes unite',
+//       subtitle:'fly as a dove',
+//       img:'be cool'
+//     }
+//   })
+// })
+
+// new Carousel({
+//   $subscribe:'upward.teasers'
+// })

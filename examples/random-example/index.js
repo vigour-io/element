56ceb11a3fae0cf64fc0efe25ea4516ee72d74be
Youri Daamen <me@youz.io>
2015-10-22 00:51:23 +0200

    dirty fixes subscribe

--- examples/random-example/index.js
+++ examples/random-example/index.js
@@ -3,22 +3,6 @@ require('./style.less')
 var Observable = require('vjs/lib/observable')
 var Element = require('../../')
 
-
-var Operator = require('vjs/lib/operator')
-
-Operator.prototype.define({
-  generateConstructor (){
-    return function(val, event, parent, key){
-      if( parent instanceof Element){
-        parent.on(function(){
-          this.val
-        })
-      }
-      return Operator.apply(this, arguments)
-    }
-  }
-})
-
 Observable.prototype.inject(require('vjs/lib/operator/add'))
 Observable.prototype.inject(require('vjs/lib/operator/subscribe'))
 Element.prototype.inject(require('../../lib/property/text'))
@@ -26,39 +10,58 @@ Element.prototype.inject(require('../../lib/property/text'))
 var Item = new Element({
   titleField: {
     text: {
-      $: 'parent.parent.title'
+      $: 'upward.title'
     }
   },
-  subtitleField:{
-    text: {
-      $: 'parent.parent.subtitle'
+  subtitleField: {
+    nested: {
+      nested: {
+        text: {
+          $: 'upward.subtitle'
+        }
+      }
+    }
+  },
+  desc: {
+    nested: {
+      nested: {
+        text: {
+          $: 'upward.description'
+        }
+      }
     }
   }
 }).Constructor
 
+var data = new Observable({
+  content: {
+    one: {
+      title: 'one',
+      subtitle: 'studje',
+      description: 'loremipsum'
+    },
+    two: {
+      title: 'two',
+      subtitle: 'nerdje',
+      description: 'loremipsum'
+    },
+    three: {
+      title: 'three',
+      subtitle: 'mannetje',
+      description: 'loremipsum'
+    },
+    four: {
+      title: 'four',
+      subtitle: 'gek',
+      description: 'loremipsum'
+    }
+  }
+})
+
 var app = new Element({
   node: document.body,
-  val: new Observable({
-    content: {
-      one: {
-        title: 'one',
-        subtitle: 'studje'
-      },
-      two: {
-        title: 'two',
-        subtitle: 'nerdje'
-      },
-      three: {
-        title: 'three',
-        subtitle: 'mannetje'
-      },
-      four: {
-        title: 'four',
-        subtitle: 'gek'
-      }
-    }
-  }),
-  info: {
+  val: data,
+  info1:{
     holder: {
       ChildConstructor: Item,
       $: 'upward.content'
@@ -66,109 +69,33 @@ var app = new Element({
   }
 })
 
-// console.log('>>', app.info.holder.val)
+setInterval(() => {
+  var numberOfFields = findFields(data)
+  var randomIndex = Math.ceil(Math.random() * numberOfFields)
+  findFields(data, randomIndex)
+},1000)
 
-// console.log('>>', app.info.holder.val)
+function findFields(obj, random, count){
+  if(!count){
+    count = {cnt:0}
+  }
+  var field = 'random!' + Math.random()
+  obj.each((prop,key) => {
+    if(typeof prop.val === 'string'){
+      let cnt = ++count.cnt
+      if(random){
+        if(cnt === random){
+          // console.log(cnt, prop.path)
+          prop.val = field
+        }else if(Math.random() > 0.5){
+          field = prop.val
+        }
+      }
+    }else{
+      findFields(prop,random,count)
+    }
+  })
+  return count.cnt
+}
 
-// var ref = new Observable({
-//   child: {
-//     nested: {
-//       title: 'ballz'
-//     }
-//   }
-// })
-
-// app.set({
-//   content: ref,
-
-// })
-
-// var Item = new Element({
-
-// }).Constructor
-
-// var GridItem = new Element({
-//   title: {
-//     text: ''
-//   },
-//   $add: {
-//     $: 'episodes'
-//   },
-//   properties: Item
-// }).Constructor
-
-// var Grid = new Element({
-//   $: 'seasons',
-//   // $map: function (season, key) {
-//   //   if (typeof key === 'number') {
-//   //     return new GridItem({
-//   //       val: season,
-//   //       title: {
-//   //         text: {
-//   //           $: 'title'
-//   //         }
-//   //       }
-//   //     })
-//   //   }
-//   // },
-//   properties:new GridItem({
-//     //val: season,
-//     title: {
-//       text: {
-//         $: 'title'
-//       }
-//     }
-//   })
-// }).Constructor
-
-
-// console.log('check', app.val)
-//   // app.set({
-//   //   info: {
-//   //     title:'funfun'
-//   //   },
-//   //   nerdjes: {
-//   //     $: 'parent.info'
-//   //   }
-//   // })
-
-// // console.log(app.nerdjes.val.title.val)
-
-// // var Slide = new Element({
-// //   ChildConstructor:false,
-// //   properties:{
-// //     title:TextElement,
-// //     subtitle:TextElement,
-// //     img:ImgElement
-// //   }
-// // cache.title
-// // }).Constructor
-
-// // var Carousel = new Element({
-// //   ChildConstructor:Slide
-// // }).ChildConstructor
-
-// // app.set({
-// //   carousel:new Carousel({
-// //     //episodes bijv
-// //     1:{
-// //       title:'nerdjes unite',
-// //       subtitle:'fly as a dove',
-// //       img:'be cool'
-// //     },
-// //     2:{
-// //       title:'nerdjes unite',
-// //       subtitle:'fly as a dove',
-// //       img:'be cool'
-// //     },
-// //     3:{
-// //       title:'nerdjes unite',
-// //       subtitle:'fly as a dove',
-// //       img:'be cool'
-// //     }
-// //   })
-// // })
-
-// // new Carousel({
-// //   $subscribe:'upward.teasers'
-// // })
+// findField(data)

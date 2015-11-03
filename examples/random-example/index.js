54c0c2f78b66da23459be16231e6e5c5fcc24222
Youri Daamen <me@youz.io>
2015-11-03 21:43:02 +0100

    fixed text updated example

--- examples/random-example/index.js
+++ examples/random-example/index.js
@@ -1,5 +1,4 @@
 require('./style.less')
-  // var app = require('../../lib/app')
 var Observable = require('vjs/lib/observable')
 var Element = require('../../')
 
@@ -9,49 +8,119 @@ Observable.prototype.inject(require('vjs/lib/operator/subscribe'))
 Element.prototype.inject(require('../../lib/property/text'))
 
 var Item = new Element({
-  key:'original',
-  text:{
-    $:'upward.title'
+  key: 'original',
+  text: {
+    $: '../title'
   },
-  nested:{
-
+  nested: {
+    text: {
+      $: '../../subtitle'
+    }
+  },
+  powertitle: {
+    text: {
+      $: '../../power'
+    }
   }
 }).Constructor
 
-var data = new Observable({
-  one:{
-    title:'blurf'
-  },
-  two:{
-    title:'smurt'
-  }
-})
+var data = new Observable()
+
+var count = 1
+while (count <= 10) {
+  let key = 'el' + count
+  data.setKey(key, {
+    title: key + '-title',
+    subtitle: key + '-subtitle',
+    power: key + '-powertitle'
+  })
+  count++
+}
 
 var app = new Element({
-  node:document.body,
-  ChildConstructor:Item,
-  $transform:data
+  node: document.body,
+  ChildConstructor: Item,
+  $transform: data
 })
 
-console.log('one:data-title',app._cache.one.val.title.val)
-console.log('one:text',app._cache.one.text.val)
-console.log('one:node',app._cache.one.node)
-console.log('one:node',app._cache.one.node.base === app._cache.one)
-
-console.log('two:data-title',app._cache.two.val.title.val)
-console.log('two:text',app._cache.two.text.val)
-console.log('two:node',app._cache.two.node)
-console.log('two:node',app._cache.two.node.base === app._cache.two)
-
-console.log(app._cache.one.text === app._cache.two.text)
-
-console.log('---->',app._cache.one.nested.node)
-
-// app._cache.two.val.title.val = 'TEST ON TWO'
 // setInterval(function(){
-//   data.two.title.set('two' + Math.random())
-// },500)
+//   data.each(function(prop, key){
+//     prop.set({
+//       title:key + '-title:' + Math.random(),
+//       subtitle:key + '-subtitle:' + Math.random(),
+//       power:key + '-powertitle:' + Math.random()
+//     })
+//   })
+// },200)
 
-// setInterval(function(){
-//   data.one.title.set('one' + Math.random())
-// },1000)
+// require('./style.less')
+//   // var app = require('../../lib/app')
+// var Observable = require('vjs/lib/observable')
+// var Element = require('../../')
+
+// Observable.prototype.inject(require('vjs/lib/operator/add'))
+// Observable.prototype.inject(require('vjs/lib/operator/subscribe'))
+// Element.prototype.inject(require('../../lib/property/text'))
+
+// var Item = new Element({
+//   key:'original',
+//   titleField: {
+//     text: {
+//       $: 'upward.title'
+//     }
+//   }//,
+//   // subtitleField: {
+//   //   nested: {
+//   //     nested: {
+//   //       text: {
+//   //         $: 'upward.subtitle'
+//   //       }
+//   //     }
+//   //   }
+//   // },
+//   // desc: {
+//   //   nested: {
+//   //     nested: {
+//   //       text: {
+//   //         $: 'upward.description'
+//   //       }
+//   //     }
+//   //   }
+//   // }
+// }).Constructor
+
+// var data = new Observable({
+//   content: {
+//     one: {
+//       title: 'one',
+//       subtitle: 'studje',
+//       description: 'loremipsum'
+//     },
+//     two: {
+//       title: 'two',
+//       subtitle: 'nerdje',
+//       description: 'loremipsum'
+//     },
+//     three: {
+//       title: 'three',
+//       subtitle: 'mannetje',
+//       description: 'loremipsum'
+//     },
+//     four: {
+//       title: 'four',
+//       subtitle: 'gek',
+//       description: 'loremipsum'
+//     }
+//   }
+// })
+
+// var app = new Element({
+//   node: document.body,
+//   val: data,
+//   holder: {
+//     ChildConstructor: Item,
+//     $: 'upward.content'
+//   }
+// })
+
+// data.content.two.title.val = 'POWER'

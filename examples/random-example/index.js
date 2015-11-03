7bccbfc26d892a462e1d50af2bbc7473215d9fe7
Youri Daamen <me@youz.io>
2015-11-03 17:55:46 +0100

    fix in text

--- examples/random-example/index.js
+++ examples/random-example/index.js
@@ -4,132 +4,54 @@ var Observable = require('vjs/lib/observable')
 var Element = require('../../')
 
 Observable.prototype.inject(require('vjs/lib/operator/add'))
+Observable.prototype.inject(require('vjs/lib/operator/transform'))
 Observable.prototype.inject(require('vjs/lib/operator/subscribe'))
 Element.prototype.inject(require('../../lib/property/text'))
 
 var Item = new Element({
-  // val:new Observable(),
   key:'original',
-  titleField: {
-    text: {
-      $: 'upward.title'
-    }
+  text:{
+    $:'upward.title'
   },
-  // subtitleField: {
-  //   nested: {
-  //     nested: {
-  //       text: {
-  //         $: 'upward.subtitle'
-  //       }
-  //     }
-  //   }
-  // },
-  // desc: {
-  //   nested: {
-  //     nested: {
-  //       text: {
-  //         $: 'upward.description'
-  //       }
-  //     }
-  //   }
-  // }
+  nested:{
+
+  }
 }).Constructor
 
 var data = new Observable({
-  content: {
-    one: {
-      title: 'one',
-      // subtitle: 'studje',
-      // description: 'loremipsum'
-    },
-    two: {
-      title: 'two',
-      // subtitle: 'nerdje',
-      // description: 'loremipsum'
-    },
-    three: {
-      title: 'three',
-      // subtitle: 'mannetje',
-      // description: 'loremipsum'
-    },
-    four: {
-      title: 'four',
-      // subtitle: 'gek',
-      // description: 'loremipsum'
-    }
+  one:{
+    title:'blurf'
+  },
+  two:{
+    title:'smurt'
   }
 })
 
-var item = new Item({
-    // text:'1',
-    key:'item',
-    val:data.content.one
-  })
-
 var app = new Element({
-  key:'APP',
-  node: document.body,
-  val: data,
-  // item1:new Item({
-  //   // text:'1',
-  //   key:'itemOne'//,
-  //   val:data.content.one
-  // }),
-  // item2:item
-  // item2:new Item({
-  //   // text:2,
-  //   key:'itemTwo',
-  //   val:data.content.two
-  // }),
-  // item3:new Item({
-  //   val:data.content.three
-  // }),
-  // item4:new Item({
-  //   val:data.content.four
-  // })
-  // info1:{
-    holder: {
-      ChildConstructor: Item,
-      $: 'upward.content'
-    }
-  // }
-  // pattern:{text:'pattern'},
-  // notpattern:{text:'notpattern'}
+  node:document.body,
+  ChildConstructor:Item,
+  $transform:data
 })
 
-// app.item1.val = data.content.one
+console.log('one:data-title',app._cache.one.val.title.val)
+console.log('one:text',app._cache.one.text.val)
+console.log('one:node',app._cache.one.node)
+console.log('one:node',app._cache.one.node.base === app._cache.one)
 
-// app.holder.val.one.val = data.content.two
-console.log('RESULT',app.holder.val.one.titleField.text.val)
+console.log('two:data-title',app._cache.two.val.title.val)
+console.log('two:text',app._cache.two.text.val)
+console.log('two:node',app._cache.two.node)
+console.log('two:node',app._cache.two.node.base === app._cache.two)
 
-// // var toggle = 0
-// setInterval(() => {
-//   // data.content[toggle === 1 ? 'one' : 'two'].title.val = Math.random()
-//   // toggle = toggle === 1 ? 0 : 1
-//   var numberOfFields = findFields(data)
-//   var randomIndex = Math.ceil(Math.random() * numberOfFields)
-//   findFields(data, randomIndex)
+console.log(app._cache.one.text === app._cache.two.text)
+
+console.log('---->',app._cache.one.nested.node)
+
+// app._cache.two.val.title.val = 'TEST ON TWO'
+// setInterval(function(){
+//   data.two.title.set('two' + Math.random())
+// },500)
+
+// setInterval(function(){
+//   data.one.title.set('one' + Math.random())
 // },1000)
-
-// function findFields(obj, random, count){
-//   if(!count){
-//     count = {cnt:0}
-//   }
-//   var field = 'random!' + Math.random()
-//   obj.each((prop,key) => {
-//     if(typeof prop.val === 'string'){
-//       let cnt = ++count.cnt
-//       if(random){
-//         if(cnt === random){
-//           // console.log(cnt, prop.path)
-//           prop.val = field
-//         }else if(Math.random() > 0.5){
-//           field = prop.val
-//         }
-//       }
-//     }else{
-//       findFields(prop,random,count)
-//     }
-//   })
-//   return count.cnt
-// }

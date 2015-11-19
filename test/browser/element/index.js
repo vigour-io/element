5f79d430c3618ca704418bc5871005b9e4bd5abe
Youri Daamen <me@youz.io>
2015-11-19 13:24:24 +0100

    added insertBefore

--- test/browser/element/index.js
+++ test/browser/element/index.js
@@ -1,11 +1,15 @@
-var Element = require( '../../../lib/element/' )
+var Element = require('../../../lib/element/')
 
-describe( 'Creating basic Element', function(){
-	require('./basic')
+describe('Creating basic Element', function () {
+  require('./basic')
 })
 
-describe( 'Creating instances of Elements', function(){
-	require('./instances')
+describe('Creating instances of Elements', function () {
+  require('./instances')
+})
+
+describe('Creating, and overwriting nodes', function () {
+  require('./node')
 })
 
 require('./remove')

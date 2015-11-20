27d0adb39d7998d1333b297c23bd54b1d21b60c8
Youri Daamen <me@youz.io>
2015-11-20 18:33:27 +0100

    added test

---
+++ examples/properties/index.js
@@ -0,0 +1,42 @@
+require('./style.less')
+
+var Observable = require('vigour-js/lib/observable')
+var Element = require('../../lib/element')
+var app = require('../../lib/app')
+
+var Property = require('../../lib/property')
+
+Property.prototype.inject(
+  require('../../lib/animation'),
+  require('vigour-js/lib/operator/subscribe')
+)
+
+Element.prototype.inject(
+  require('../../lib/property/text'),
+  require('../../lib/property/css'),
+  require('../../lib/property/size'),
+  require('../../lib/property/scroll/top'),
+  require('../../lib/property/scroll/left'),
+  require('../../lib/property/transform'),
+  require('../../lib/events/click')
+)
+
+// test properties
+var TestElem = new Element({
+  text: 'nothing',
+  properties: {
+    state: new Observable({
+      on: {
+        data () {
+          this.parent.text.val = this.val
+        }
+      }
+    })
+  }
+}).Constructor
+
+app.set({
+  test: new TestElem({
+    state: 1
+  })
+})

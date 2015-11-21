39dc458677d965697cc38b8c9121295cce40ce9f
Danillo Corvalan <danillo.corvalan@gmail.com>
2015-11-21 16:05:21 +0100

    player abstraction and basic html5 player

--- examples/player/html5/index.js
+++ examples/player/html5/index.js
@@ -2,63 +2,13 @@
 
 // var Player = require('../../../lib/player/')
 var app = require('../../../lib/app')
-var Element = require('../../../lib/element')
 
-Element.prototype.inject(require('../../../lib/events/render'))
+var Player = require('../../../lib/player/')
+var myPlayer = new Player({
+  inject: require('../../../lib/player/players/html5')
+})
 
-var Property = require('../../../lib/property')
-
-var Observable = require('vigour-js/lib/observable')
-
-var Player = new Element({
-  play: new Observable(),
-  time: new Observable(),
-  duration: new Observable(),
-  src: new Observable()
-}).Constructor
-
-var Html5Player = new Player({
-  node: 'video',
-  on: {
-    render () {
-      console.error('heyo')
-    }
-  },
-  play: {
-    on: {
-      data (data) {
-        console.log('data has been changed in Implementation', data)
-        var node = this.parent.node
-        var key = 'autoplay'
-        var val = this.val
-
-        if (val) {
-          node.play()
-        } else {
-          node.pause()
-        }
-      }
-    }
-  },
-  src: {
-    on: {
-      data (data) {
-        console.log('SRC changed on implementation')
-        var node = this.parent.node
-        var key = this.key
-        var val = this.val
-
-        if (val) {
-          node.setAttribute(key, val)
-        } else {
-          node.removeAttribute(key)
-        }
-      }
-    }
-  }
-}).Constructor
-
-var myPlayer = new Html5Player({
+myPlayer.set({
   play: false,
   src: 'http://localhost:8000/rahh.mov'
 })
@@ -67,49 +17,21 @@ app.set({
   player: myPlayer
 })
 
-// myPlayer.emit('render')
-
 setTimeout(function () {
   myPlayer.play.val = true
 }, 2000)
 
 setTimeout(function () {
-  myPlayer.play.val = false
+  // myPlayer.play.val = true
+  myPlayer.time.val = 0.9
+  myPlayer.volume.val = 0
+}, 3000)
+
+setTimeout(function () {
+  // myPlayer.volume.val = 0.1
+  // myPlayer.time.val = 0.9
 }, 4000)
 
-// var Html5Player = new Element({
-//   node: 'video',
-//   properties: {
-//     play: new Property({
-//       on: {
-//         data: function (data) {
-//           var node = this.parent.node
-//           var key = 'autoPlay'
-//           var val = this.val
-//
-//           if (val) {
-//             node.setAttribute(key, val)
-//           } else {
-//             node.removeAttribute(key)
-//           }
-//         }
-//       }
-//     }),
-//
-//     src: new Property({
-//       on: {
-//         data: function (data) {
-//           var node = this.parent.node
-//           var key = this.key
-//           var val = this.val
-//
-//           if (val) {
-//             node.setAttribute(key, val)
-//           } else {
-//             node.removeAttribute(key)
-//           }
-//         }
-//       }
-//     })
-//   }
-// }).Constructor
+// setTimeout(function () {
+//   myPlayer.play.val = false
+// }, 6000)

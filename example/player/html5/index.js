a880bf69cb2e4769dd91943ddee465f0f2de86e8
Danillo Corvalan <danillo.corvalan@gmail.com>
2015-11-23 17:57:26 +0100

    WIP - video time update

--- example/player/html5/index.js
+++ example/player/html5/index.js
@@ -1,31 +1,53 @@
 'use strict'
 
 var App = require('../../../lib/app')
+var Element = require('../../../lib/element')
 var app = new App({
   node: document.body
 })
 
 var Player = require('../../../lib/player/')
+
 var myPlayer = new Player({
   inject: require('../../../lib/player/players/html5')
 })
-
 myPlayer.set({
   play: false,
   src: 'http://localhost:8000/rahh.mov',
   volume: 1
 })
 
-app.set({
-  player: myPlayer
+var secondPlayer = new Player({
+  inject: require('../../../lib/player/players/html5')
+})
+secondPlayer.set({
+  src: 'http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4',
+  volume: 0,
+  time: {
+    on: {
+      data (data) {
+        console.log('RAHH', this.val);
+      }
+    }
+  }
 })
 
-setTimeout(function () {
-  myPlayer.play.val = true
-}, 2000)
+app.set({
+  // player: myPlayer,
+  secondPlayer: secondPlayer,
+  label: new Element({
+    node: 'h1',
+    inject: require('../../../lib/property/text'),
+    text: secondPlayer.time
+  })
+})
 
-setTimeout(function () {
-  // myPlayer.play.val = true
-  myPlayer.time.val = 0.9
-  myPlayer.volume.val = 0.3
-}, 3000)
+// setTimeout(function () {
+//   myPlayer.play.val = true
+// }, 2000)
+
+// setTimeout(function () {
+//   // myPlayer.play.val = true
+//   myPlayer.time.val = 0.9
+//   myPlayer.volume.val = 0.3
+// }, 3000)

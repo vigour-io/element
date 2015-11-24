9a2826ed9803d08ade4d9b6d89f53746f314d1b2
renancarvalho <renansscarvalho@gmail.com>
2015-11-24 16:42:47 +0100

    added some tests for navigation events

---
+++ test/browser/events/nav/index.js
@@ -0,0 +1,15 @@
+describe('On Arrow  Down', function () {
+  require('./down')
+})
+
+describe('On Arrow Up', function () {
+  require('./up')
+})
+
+describe('On Arrow Left', function () {
+  require('./left')
+})
+
+describe('On Arrow Right', function () {
+  require('./right')
+})

b6e22679ed3dc88f8f2d0751d000793cba92147b
Danillo Corvalan <danillo.corvalan@gmail.com>
2015-11-25 19:29:45 +0100

    ads player example

--- example/player/html5/index.js
+++ example/player/html5/index.js
@@ -1,5 +1,7 @@
 'use strict'
-
+var Observable = require('vigour-js/lib/observable')
+Observable.prototype.inject(require('vigour-js/lib/operator/subscribe'))
+Observable.prototype.inject(require('vigour-js/lib/operator/transform'))
 var App = require('../../../lib/app')
 var app = window.app = new App({
   node: document.body
@@ -9,36 +11,74 @@ var Player = require('../../../lib/player/')
 
 var thePlayer = new Player({
   inject: require('../../../lib/player/html5'),
-  src: 'http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4',
+  // src: 'http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4',
+  src: 'http://www.sample-videos.com/video/mp4/720/big_buck_bunny_720p_10mb.mp4',
   volume: 0.1,
-  play: false
+  play: true
 })
 
-//
-// theplay.ads.set({
-//   src: 'http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4',
-//   volume: 0.1,
-//   play: true
-// })
+thePlayer.ad.set({
+  // src: 'http://html5videoformatconverter.com/data/images/happyfit2.mp4',
+  src: 'http://www.sample-videos.com/video/mp4/720/big_buck_bunny_720p_2mb.mp4',
+  play: true
+})
 
 app.set({
   thePlayer: thePlayer,
-  label: {
-    node: 'h1',
-    inject: require('../../../lib/property/text'),
-    text: thePlayer.time
+
+  progressContainer: {
+    node: 'div',
+    inject: [
+      require('../../../lib/property/attributes'),
+      require('../../../lib/property/size')
+    ],
+    width: thePlayer.videoWidth,
+    height: 30,
+
+    progress: {
+      inject: [
+        require('../../../lib/property/attributes'),
+        require('../../../lib/property/style'),
+        require('../../../lib/property/size')
+      ],
+      width: {
+        $: '../../thePlayer.time',
+        $transform (val) {
+          return ~~(val * 100) + '%'
+        }
+      },
+      height: 8,
+      style: {
+        backgroundColor: 'red'
+      }
+    },
+
+    bufferProgress: {
+      inject: [
+        require('../../../lib/property/attributes'),
+        require('../../../lib/property/style'),
+        require('../../../lib/property/size')
+      ],
+      width: {
+        $: '../../thePlayer.buffer',
+        $transform (val) {
+          return ~~(val * 100) + '%'
+        }
+      },
+      height: 8,
+      style: {
+        backgroundColor: '#03A9F4'
+      }
+    }
   },
-  buffer: {
-    inject: require('../../../lib/property/text'),
-    text: thePlayer.buffer
-  },
+
   play: {
     node: 'button',
     inject: [
       require('../../../lib/property/text'),
       require('../../../lib/events/click')
     ],
-    text: 'play',
+    text: 'play/pause',
     on: {
       click () {
         thePlayer.toggle()

e05b52ea59dfcab1eaaa12cbd49bc208507b3e61
nikoloza <nika.tomadze@gmail.com>
2015-12-02 22:07:33 +0100

    vjs version up

--- example/animation-nika/index.js
+++ example/animation-nika/index.js
@@ -1,15 +1,14 @@
 'use strict'
 
 require('./nika.less')
-var Element = require('../../lib/element')
 var App = require('../../lib/app')
 
 var Property = require('../../lib/property')
 Property.prototype.inject(
-  // require('../../lib/animation'),
-  // require('vigour-js/lib/operator/add')
+  require('../../lib/animation')
 )
 
+var Element = require('../../lib/element')
 Element.prototype.inject(
   require('../../lib/property/transform'),
   require('../../lib/property/css'),
@@ -19,60 +18,37 @@ Element.prototype.inject(
 var Nika = new Element({
   css: 'nika',
   head: {},
-  // body: {},
   x: {
-    val: 0
-      // $animation: 200
+    val: 0,
+    $animation: {
+      duration: 32
+    }
   },
   y: {
-    val: 0
-      // $animation: 200
+    val: 0,
+    $animation: {
+      duration: 32
+    }
   }
 }).Constructor
 
 var dirX = 0
 var dirY = 0
 var app
-var dirXs = {}
-var dirYs = {}
+// var dirXs = {}
+// var dirYs = {}
 
 var cnt = 250
 
-function loop () {
-  window.requestAnimationFrame(function () {
-    for (var i = 0; i < cnt; i++) {
-      var x = app[i].x.val + (dirXs[i] * dirX)
-      var y = app[i].y.val + (dirYs[i] * dirY)
+// var winX = window.innerWidth
+// var winY = window.innerHeight
 
-      if (x > window.innerWidth) {
-        dirXs[i] = dirX > 0 ? -1 : 1
-      } else if (x < 0) {
-        dirXs[i] = dirX > 0 ? 1 : -1
-      }
-
-      if (y > window.innerHeight) {
-        dirYs[i] = dirY > 0 ? -1 : 1
-      } else if (y < 0) {
-        dirYs[i] = dirY > 0 ? 1 : -1
-      }
-
-      app[i].set({
-        x: x,
-        y: y
-      })
-    }
-    // Nika.prototype.x.val = Nika.prototype.x.val + dirX
-    // Nika.prototype.y.val = Nika.prototype.y.val + dirY
-    loop()
-  })
-}
-loop()
-
-app = new App({ // eslint-disable-line
+app = new App({
   node: document.body,
   on: {
     keydown (ev, event) {
-      var code = ev.keyCode
+      let code = ev.keyCode
+
       if (code === 39) {
         dirX += 1
       } else if (code === 37) {
@@ -84,18 +60,40 @@ app = new App({ // eslint-disable-line
       } else if (code === 13) {
         dirX = 0
         dirY = 0
-          // this.setKey((nikas++), new Nika())
       }
-      // if
+
+      updateIt()
     }
   }
 })
 
-for (var i = 0; i < cnt; i++) {
-  app.setKey((i), new Nika({
-    x: 5 * i * Math.cos(i / 100) + 250,
-    y: 5 * i * Math.sin(i / 100) + 250
-  }))
-  dirXs[i] = 1
-  dirYs[i] = 1
+function updateIt () {
+  for (let i = 0; i < cnt; i++) {
+    app.setKey((i), new Nika({
+      x: {
+        val: 5 * i * Math.cos(i / 100) + 250
+      },
+      y: {
+        val: 5 * i * Math.sin(i / 100) + 250
+      }
+    }))
+
+    // var x = app[i].x.val + (dirXs[i] * dirX)
+    // var y = app[i].y.val + (dirYs[i] * dirY)
+
+    // if (x > winX) {
+    //   x = dirX > 0 ? -1 : 1
+    // } else if (x < 0) {
+    //   x = dirX > 0 ? 1 : -1
+    // }
+
+    // if (y > winY) {
+    //   y = dirY > 0 ? -1 : 1
+    // } else if (y < 0) {
+    //   y = dirY > 0 ? 1 : -1
+    // }
+
+    // dirXs[i] = 1
+    // dirYs[i] = 1
+  }
 }

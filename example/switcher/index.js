41a6bcb1afa3fdfdec6ba217582593086ddf49e4
renancarvalho <renansscarvalho@gmail.com>
2015-12-03 18:05:39 +0100

    spliting examples

--- example/switcher/index.js
+++ example/switcher/index.js
@@ -30,7 +30,8 @@ Element.prototype.inject(
   require('../../lib/animation')
 )
 app.set({
-  switcher : new Switcher
+  switcher : new Switcher,
+  switcher2 : new Switcher
 })
 var container = new Element({
   css:'container',
@@ -74,4 +75,47 @@ var Container2 = new Element({
   }
 }).Constructor
 
+var containerex = new Element({
+  css:'container',
+  on:{
+    transitionEnd () {
+      console.log('removed container 1')
+      this.remove()
+    },
+    click () {
+      console.log('clicked back new')
+      var container2 = new Container2ex
+      app.switcher2.emit('switchto', [this, container2, 'left', 60]) //Also animation types (opacity, transiction)
+    }
+  },
+  x:{
+    val:0,
+    $animation:{
+
+    }
+  }
+}).Constructor
+
+var Container2ex = new Element({
+  css: 'container2',
+  on:{
+    transitionEnd () {
+      console.log('remove')
+      this.remove()
+    },
+    click () {
+      console.log('clicked back')
+      var container1 = new containerex
+      app.switcher2.emit('switchto', [this, container1, 'right', 60]) //Also animation types (opacity, transiction)
+    }
+  },
+  x:{
+    val:0,
+    $animation:{
+
+    }
+  }
+}).Constructor
+
 app.switcher.emit('add',{0: new container})
+app.switcher2.emit('add',{0: new containerex})

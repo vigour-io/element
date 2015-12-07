99c83c032db5ad8d7cba4a68d206f9b91c7db46d
renancarvalho <renansscarvalho@gmail.com>
2015-12-07 13:53:43 +0100

    Added 'togetherto' animation type.

--- example/switcher/index.js
+++ example/switcher/index.js
@@ -31,55 +31,13 @@ Element.prototype.inject(
   require('../../lib/animation')
 )
 app.set({
-  select:{
-    button1:{
-      css:'buttons',
-      node:'button',
-      text:'SwitchTo',
-      on: {
-        click() {
-          this.node.className = "selected buttons"
-          this.parent.button2.node.className = "buttons"
-          this.parent.button3.node.className = "buttons"
-          this.parent.parent.switcher.set({animationType: 'togetherto'})
-        }
-      }
-    },
-    button2:{
-      css:'buttons',
-      node:'button',
-      text:'fadeTo',
-      on: {
-        click() {
-          this.node.className = "selected buttons"
-          this.parent.button1.node.className = "buttons"
-          this.parent.button3.node.className = "buttons"
-          this.parent.parent.switcher.set({animationType: 'fadeto'})
-
-        }
-      }
-    },
-    button3:{
-      css:'buttons',
-      node:'button',
-      text:'changeTo',
-      on: {
-        click() {
-          this.node.className = "selected buttons"
-          this.parent.button1.node.className = "buttons"
-          this.parent.button2.node.className = "buttons"
-          this.parent.parent.switcher.set({animationType: 'changeto'})
-        }
-      }
-    }
-  },
   switcher : new Switcher
 })
 
 
 
 var container = new Element({
-  text:'1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19',
+  text:'1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26',
   css:'container',
     x:{
     val:0,
@@ -96,28 +54,28 @@ var container = new Element({
   on:{
     transitionEnd () {
       console.log('removed container 1')
-      // this.remove()
+      this.remove()
     },
     click () {
       console.log('clicked back new')
       var container2 = new Container2
-      app.switcher.emit(this.parent.animationType.val , [this, container2, 'left', 50])
+      app.switcher.emit('changeto' , [this, container2, 'right', 1000])
     }
   }
 }).Constructor
 
 var Container2 = new Element({
-  text:'1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19',
+  text:'1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26',
   css: 'container2',
   on:{
     transitionEnd () {
-      console.log('remove')
-      // this.remove()
+      console.log('remove container 2')
+      this.remove()
     },
     click () {
       console.log('clicked back')
       var container1 = new container
-      app.switcher.emit(this.parent.animationType.val , [this, container1, 'right', 50])
+      app.switcher.emit('changeto', [this, container1, 'left', 1000])
     }
   },
   x:{

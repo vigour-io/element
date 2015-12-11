f557f67f437f38422e24139f5e25f15310e424b8
renancarvalho <renansscarvalho@gmail.com>
2015-12-11 17:23:39 +0100

    refactoring element tests

--- test/browser/listeners/value.js
+++ test/browser/listeners/value.js
@@ -1,3 +1,46 @@
+describe('Element Event value',function () {
+  var Element = require('../../../lib/element/')
+  var fireEvent = require('../util/util').fireEvent
+  var elem
+  var spy
+  var refefenceObject
+  beforeEach(function () {
+    elem = new Element()
+    elem._on.set({
+      value: function () {
+      }
+    })
+  })
+
+  context('When declaring a value listener to an element', function () {
+    beforeEach(function () {
+      refefenceObject = new Element()
+      spy = sinon.spy(elem._on.value.fn,'val')
+    })
+
+    afterEach(function () {
+      spy.reset()
+    })
+
+    it('should get triggered if the element val is changed', function () {
+      elem.val = 1
+      expect(spy.called).to.be.true
+    })
+
+    it('should get triggered if the element val is changed to another element', function () {
+      elem.val = new Element()
+      expect(spy.called).to.be.true
+    })
+
+    it('should not get triggered when setting a new key', function () {
+      elem.set({key: 'a'})
+      expect(spy.called).to.not.be.true
+    })
+  })
+})
+
+
+
 // var Element = require('../../../lib/element')
 // var elem = new Element()
 // var valueCount

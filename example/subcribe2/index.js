4138599a5613b036a63cdaddc34cb8e3769c812c
Youri Daamen <me@youz.io>
2015-12-13 15:59:32 +0100

    better render and app

--- example/subcribe2/index.js
+++ example/subcribe2/index.js
@@ -8,7 +8,8 @@ var App = require('../../lib/app')
 Observable.prototype.inject(
   require('../../lib/animation'),
   require('vigour-js/lib/operator/subscribe'),
-  require('vigour-js/lib/operator/transform')
+  require('vigour-js/lib/operator/transform'),
+  require('vigour-js/lib/operator/add')
 )
 
 Element.prototype.inject(
@@ -23,8 +24,19 @@ Element.prototype.inject(
 
 var Item = new Element({
   text: {
-    $: '../test'
-  }
+    $: '../',
+    $add: 'smur'
+  },
+  // foo: {
+  //   text: {
+  //     $: '../../'
+  //   }
+  // },
+  // bar: {
+  //   text: {
+  //     $: '../../'
+  //   }
+  // }
 }).Constructor
 
 var obj = {}
@@ -36,28 +48,33 @@ var List = new Element({
 
 var data = new Observable({
   content: {
-    on: {
-      data: {
-        test () {
-          console.log('data')
-        }
-      },
-      property () {
-        console.log('property')
-      }
-    }
+    // on: {
+    //   data: {
+    //     test () {
+    //       console.log('data')
+    //     }
+    //   },
+    //   property () {
+    //     console.log('property')
+    //   }
+    // }
   }
 })
+
+console.time('content set')
+var event = new Event(data.content, 'data')
+event.isTriggered = true
 
 var app = new App({
   node: document.body,
   val: data,
   list: new List()
-})
+}, event)
 
-var event = new Event(data.content, 'data')
-event.isTriggered = true
-for (var i = 500; i > 0; i--) {
-  data.content.set({[i]: {test:'test'+i}}, event)
+
+for (var i = 1; i > 0; i--) {
+  data.content.set({[i]: 'test'+i }, event)
 }
+console.log('trigger!')
 event.trigger()
+console.timeEnd('content set')

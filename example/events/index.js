39ac24c6774620929c6adff1e24704ade1486f86
Youri Daamen <me@youz.io>
2015-12-29 20:39:32 +0100

    added example

---
+++ example/events/index.js
@@ -0,0 +1,33 @@
+'use strict'
+var Element = require('../../lib/element')
+var Event = require('vigour-js/lib/event')
+var Observable = require('vigour-js/lib/observable')
+var App = require('../../lib/engine/dom')
+require('./style.less')
+
+var app = new App({
+  downer: new Element({
+    text: 'downer',
+    on: {
+      down () {
+        console.log('down')
+      }
+    }
+  }),
+  upper: new Element({
+    text: 'upper',
+    on: {
+      up () {
+        console.log('up')
+      }
+    }
+  }),
+  clicker: new Element({
+    text: 'clicker',
+    on: {
+      click () {
+        console.log('click')
+      }
+    }
+  })
+})

60bb683df47998852fce4761368a17ee76f706de
Youri Daamen <me@youz.io>
2015-12-29 22:04:55 +0100

    events and props

--- example/events/index.js
+++ example/events/index.js
@@ -8,32 +8,63 @@ require('./style.less')
 Element.prototype.inject(
   require('../../lib/event/down'),
   require('../../lib/event/click'),
-  require('../../lib/event/up')
+  require('../../lib/event/up'),
+  require('../../lib/event/drag'),
+  require('../../lib/event/move')
 )
 
-var app = new App({
+var holder = new Element({
   downer: new Element({
     text: 'downer',
+    css: 'red',
     on: {
       down () {
+        this.text.val = Math.random()
         console.log('down')
       }
     }
   }),
-  upper: new Element({
-    text: 'upper',
-    on: {
-      up () {
-        console.log('up')
-      }
-    }
-  }),
-  clicker: new Element({
-    text: 'clicker',
-    on: {
-      click () {
-        console.log('click')
-      }
+  // upper: new Element({
+  //   text: 'upper',
+  //   on: {
+  //     up () {
+  //       this.text.val = Math.random()
+  //       console.log('up')
+  //     }
+  //   }
+  // }),
+  // clicker: new Element({
+  //   text: 'clicker',
+  //   on: {
+  //     click () {
+  //       this.text.val = Math.random()
+  //       console.log('click')
+  //     }
+  //   }
+  // }),
+  // dragger: new Element({
+  //   text: 'dragger',
+  //   on: {
+  //     drag () {
+  //       console.log('drag')
+  //     }
+  //   }
+  // }),
+  // mover: new Element({
+  //   text: 'mover',
+  //   on: {
+  //     move () {
+  //       console.log('move')
+  //     }
+  //   }
+  // })
+})
+
+var app = new App({
+  holder1: new holder.Constructor(),
+  holder2: new holder.Constructor({
+    downer: {
+      css:'border'
     }
   })
 })

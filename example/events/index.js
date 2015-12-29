f65271106489b2c7cea988648173d9effd957017
Youri Daamen <me@youz.io>
2015-12-29 22:33:00 +0100

    examples

--- example/events/index.js
+++ example/events/index.js
@@ -5,66 +5,67 @@ var Observable = require('vigour-js/lib/observable')
 var App = require('../../lib/engine/dom')
 require('./style.less')
 
+Observable.prototype.inject(
+  require('vigour-js/lib/operator/add')
+)
+
 Element.prototype.inject(
   require('../../lib/event/down'),
   require('../../lib/event/click'),
   require('../../lib/event/up'),
   require('../../lib/event/drag'),
-  require('../../lib/event/move')
+  require('../../lib/event/move'),
+  require('../../lib/event/render')
 )
 
 var holder = new Element({
   downer: new Element({
     text: 'downer',
-    css: 'red',
+    // css: 'red',
     on: {
-      down () {
+      mousedown () {
         this.text.val = Math.random()
-        console.log('down')
+        console.log('down', this.path, '->', this.text.path)
       }
     }
   }),
-  // upper: new Element({
-  //   text: 'upper',
-  //   on: {
-  //     up () {
-  //       this.text.val = Math.random()
-  //       console.log('up')
-  //     }
-  //   }
-  // }),
-  // clicker: new Element({
-  //   text: 'clicker',
-  //   on: {
-  //     click () {
-  //       this.text.val = Math.random()
-  //       console.log('click')
-  //     }
-  //   }
-  // }),
-  // dragger: new Element({
-  //   text: 'dragger',
-  //   on: {
-  //     drag () {
-  //       console.log('drag')
-  //     }
-  //   }
-  // }),
-  // mover: new Element({
-  //   text: 'mover',
-  //   on: {
-  //     move () {
-  //       console.log('move')
-  //     }
-  //   }
-  // })
+  upper: new Element({
+    text: 'upper',
+    on: {
+      mouseup () {
+        this.text.val = Math.random()
+        console.log('up', this.path, '->', this.text.path)
+      }
+    }
+  }),
+  clicker: new Element({
+    text: 'clicker',
+    on: {
+      click () {
+        this.text.val = Math.random()
+        console.log('click', this.path, '->',this.text.path)
+      }
+    }
+  }),
+  dragger: new Element({
+    text: 'dragger',
+    on: {
+      drag () {
+        console.log('drag')
+      }
+    }
+  }),
+  mover: new Element({
+    text: 'mover',
+    on: {
+      move () {
+        console.log('move')
+      }
+    }
+  })
 })
 
 var app = new App({
-  holder1: new holder.Constructor(),
-  holder2: new holder.Constructor({
-    downer: {
-      css:'border'
-    }
-  })
+  holder1: new holder.Constructor()//,
+  // holder2: new holder.Constructor()
 })

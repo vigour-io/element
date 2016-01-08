4b14bbe6bb2052827223ecad69c4fc8520b37ec9
Jim de Beer <jim@vigour.io>
2016-01-08 18:49:02 +0100

    update

--- example/episodepage/index.js
+++ example/episodepage/index.js
@@ -210,25 +210,6 @@ setTimeout(function(){
 // global.show = show
 // global.show2 = show2
 
-global.wl = function walklisteners (obs, cnt) {
-  var x
-  if (!cnt) {
-    x = true
-    cnt = { cnt: 0 }
-  }
-  // cnt.cnt++
-  obs.each(function (p, key) {
-    walklisteners(p, cnt)
-  })
-  if (obs._on) {
-    if (obs._on.data && obs._on.data.attach) {
-      cnt.cnt++
-    }
-  }
-  if (x) {
-    console.log('listeners!', cnt.cnt)
-  }
-}
 
 
 global.show = show

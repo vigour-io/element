7e56b587eace322ef0d75f75dd3c2497dae73b1d
Jim de Beer <jim@vigour.io>
2016-01-22 01:41:08 +0100

    tests

--- example/a/index.js
+++ example/a/index.js
@@ -13,6 +13,9 @@ var a = global.a = new Observable({
 
 var thing = new Element({
   text: a,
+  xxx: {
+    text:'xxxx'
+  },
   css: {
     $: 'flups'
   }
@@ -34,8 +37,11 @@ var thing2 = new Element({
   flups: new thing.Constructor()
 })
 
+// var bla = new thing2.flups.Constructor()
+
 app.set({
-  tx: new thing2.Constructor()
+  tx: new Element({})
+  // tx: new thing2.Constructor()
 })
 
 var m = {}
@@ -43,26 +49,65 @@ console.log(app.tx.$map(void 0, void 0, m), m)
 // console.log(app.tx.storedmap)
 
 app.tx.set({
-  bla: {
-    text: {
-      $: 'gurkens'
-    }
-  },
-  xx: y,
-  yy: new x.Constructor()
+  // bla: {
+  //   text: {
+  //     $: 'gurkens'
+  //   }
+  // },
+  // xx: y,
+  // yy: new x.Constructor()
 })
 
 var m = {}
 console.log('?', JSON.stringify(app.tx.$map(void 0, void 0, m), false, 2 ), m)
 // lets fix!
-app.tx.val = a
+// app.tx.val = a
 
 console.log('lets inject')
 var b = new Element({
-  type:'x',
-  inject: new Element({ gurk : { text: true }})
+  bitchez: {
+    living: {
+      room: {
+        text: 'ugh'
+      }
+    }
+  },
+  inject: new thing2.Constructor({
+    bitchez: {
+      noway: {
+        text: 'gozee'
+      }
+    },
+    xxx: true,
+    properties: {
+      gurk: function (val) {
+        this.gurk = val
+      }
+    },
+    define: {
+      yuzi: function () {
+
+      }
+    },
+    gurk: 'smoops',
+    ChildConstructor: new Element({ blurf: {} })
+  })
 })
 
 app.tx.set({
-  bb: b
+  bb: new b.Constructor({
+    ChildConstructor: {
+      james: { text: 'yo!' }
+    }
+  })
 })
+
+app.tx.set({
+  bb: new b.Constructor({
+    ChildConstructor: {
+      james: { text: 'yex' } //check maybe allways make new?
+    }
+  })
+})
+
+console.log(b.gurk, b, app.tx.bb.ChildConstructor.prototype.james.text.val)

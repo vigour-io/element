a1d98ec209e3e255d95fe7253e87231355313099
Jim de Beer <jim@vigour.io>
2016-01-25 02:21:23 +0100

    fixes

--- example/a/s.js
+++ example/a/s.js
@@ -11,23 +11,32 @@ var hub = global.hub = new Hub({
   adapter: {
     inject: require('vigour-hub/lib/protocol/websocket'),
     websocket: {
-      val: 'ws://localhost:3031'
-      // val: 'ws://37.48.93.68:9000'
-    }
+      val: 'ws://localhost:3031',
+      connected: {
+        on: {
+          data () {
+            console.log('yo connect!')
+          }
+        }
+      }
+    },
+    scope: 'james'
   },
   jobs: {}
 })
 
-
 app.set({
   jobs: new Element({
-    $: true,
-    bla: {
-      $collection: true,
-      Child: {
-        text: { $: 'filename' }
-      },
+    $collection: 'jobs',
+    Child: {
+      type: 'li',
+      bla: {
+        $collection: true,
+        Child: {
+          text: { $: true }
+        }
+      }
     },
-    val: hub.jobs
+    val: hub
   })
 })

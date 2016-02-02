55a480b06db2690c40948c57724caaef30a16851
Jim de Beer <jim@vigour.io>
2016-02-02 18:57:37 +0100

    start of subscription to the hub, added basic events

--- example/refactor/index.js
+++ example/refactor/index.js
@@ -2,113 +2,80 @@
 var Element = require('../../lib')
 var Observable = require('vigour-js/lib/observable')
 
-var app = new Element({
+// temp solution
+var Syncable = require('vigour-hub/lib/syncable')
+var _set = Syncable.prototype.set
+var _on = Syncable.prototype.on
+
+Syncable.prototype.define({
+  set () {
+    var r = _set.apply(this, arguments)
+    if (this._on && this._on.data.base) {
+      for (let i in this._on.data.base) {
+        let prop = this._on.data.base[i]
+        if (prop instanceof Element) {
+          prop.patch()
+          break
+        }
+      }
+    }
+    return r
+  },
+  on (type, val) {
+    if (val.$map) {
+      this.$(val.$map())
+    }
+    return _on.apply(this, arguments)
+  }
+})
+
+// Syncable.prototype.set({
+  // on attach add!
+// })
+
+var Hub = require('vigour-hub')
+var hub = global.hub = new Hub({
+  adapter: {
+    inject: require('vigour-hub/lib/protocol/websocket'),
+    websocket: 'ws://37.48.93.68:5051'
+  }
+})
+
+// hub.$({
+//   shows: {
+//     '*': {
+//       title: { val: true },
+//       img: { val: true }
+//     }
+//   }
+// })
+
+var app = global.app = new Element({
   DOM: document.body
 })
 
-var list = new Observable({
-  shows: {
-    1: { title: 'xxx' },
-    2: { title: 'yyy' }
-  }
-})
-
-var list2 = new Observable({
-  shows: {
-    1: { title: 'hhh' },
-    2: { title: 'hihih' }
-  }
-})
-
-var bla = new Observable({
-  title: {
-    val: 'YUZI!',
-    blurf: 'hahahaha!',
+var Shows = new Element({
+  Child: {
+    // text: {
+    //   $: 'title'
+    // },
+    img: {
+      type: 'img',
+      src: { $: 'img' },
+      // $transform (val) {
+      //   return val && `https://imgmtvplay-a.akamaihd.net/image/400/300?url=${val}`
+      // }
+    },
+    title: {
+      text: { $: 'title' }
+    }
   },
-  x: 20
-})
-
-// of course normal obs dont rly update have to handle that with subs --
-// will be done subs is injectable enhances set boom
-
-// update from 'sbscribeble or something give it a good name -- that can be used anywhere'
-// this will make update buble up! amaze ballz!
-console.clear()
-
-var Gurk = new Element({
-  type: 'br',
-  css: {
-    james: 'xxx'
-  }
-}).Constructor
-
-var Smuts = new Element({
-  type: 'br',
-  css: {
-    james: 'xxx'
-  }
+  $collection: 'shows'
 }).Constructor
 
 app.set({
-  on: {
-    click () {
-      console.log('whatttt?', this.path)
-    }
-  },
-  bla: new Gurk({
-    type: 'img',
-    bla: new Smuts(),
-    x: bla.x, // DOES NOT LISTEN YET WILL BE FIXED! e.g. add on -- only when ref (else it just fires)
-    attributes: {
-      src: { $: 'title.blurf' }
-    },
-    on: {
-      click (ev) {
-        this.set({
-          type: 'input',
-          attributes: null,
-          value: bla.x
-        })
-      },
-      keyup (ev) {
-        // fix origin for property ofcourse
-        this.value.origin.val = ev.target.value
-        this.patch()
-      }
-    },
-    // css: {
-    //   yuzi: {
-    //     $: 'title',
-    //     jurk: {
-    //       $: 'blurf'
-    //     }
-    //   }
-    //   // make it possible to put stuff on props jus tuse the same shit
-    // },
-    val: bla
-  }),
-  bla2: {
-    Child: {
-      text: { $: 'title' },
-      w: 100
-    },
-    // so each collection has to have its own renderthing
-    $collection: 'shows',
-    val: list
-  }
+  shows: new Shows(hub)
 })
 
-console.clear()
-for (var i = 0; i < 10; i++) {
-  list2.shows.set({
-    [i]: {
-      title: 'hux'
-    }
-  }, false)
-}
+//// ws://37.48.93.68:5051
 
-setTimeout(function () {
-  app.bla2.val = list2
-}, 500)
-
-// rename vjs to observable

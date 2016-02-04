08db13ce13d143902a08b7233484cef73344ab6b
Youri Daamen <me@youz.io>
2016-02-04 23:12:35 +0100

    more carousel action with focus data

--- example/flex/index.js
+++ example/flex/index.js
@@ -9,10 +9,10 @@ var app = global.app = new Element({
   DOM: document.body
 })
 
-var cnt = 0
+Observable.prototype.inject(require('vigour-element/lib/subscription/stamp'))
 
 var list = new Observable({
-  focus: 'one',
+  focus: 'three',
   one: {
     title: 'one'
   },
@@ -27,9 +27,12 @@ var list = new Observable({
   }
 })
 
+var cnt = 0
+var arr = ['one', 'two', 'three', 'four']
+
 app.set({
+  $: true,
   carousel: new Carousel({
-    $: true,
     val: list,
     items: {
       Child: {
@@ -38,19 +41,6 @@ app.set({
         }
       },
       $collection: true
-      // one: {
-      //   html: 'one'
-      // },
-      // two: {
-      //   html: 'two'
-      // },
-      // three: {
-      //   html: 'three'
-      // },
-      // four: {
-      //   html: 'four'
-      // },
-      // focusChild: 'one'
     }
   }),
   button: {
@@ -60,23 +50,24 @@ app.set({
     html: 'left',
     on: {
       click (e, event) {
-        app.carousel.items.setKey('animateLeft', ++cnt)
-        app.carousel.items.postpone('carousel', function () {
-          var obj = {}
-          var length = -1
-          console.log('---->',this)
-          this.each(function (prop, key) {
-            console.log('??---->',key)
-            var index = prop.flex.order.val
-            obj[index] = prop
-            length++
-          })
-          obj[0].flex.setKey('order', length)
-          for (var i = 1; i <= length; i++) {
-            obj[i].flex.setKey('order', i - 1)
-          }
-          this.setKey('marginLeft', cnt)
-        }, 300)
+        list.set({focus: arr[cnt++] || arr[cnt = 0]})
+        // app.carousel.items.setKey('animateLeft', ++cnt)
+        // app.carousel.items.postpone('carousel', function () {
+        //   var obj = {}
+        //   var length = -1
+        //   console.log('---->',this)
+        //   this.each(function (prop, key) {
+        //     console.log('??---->',key)
+        //     var index = prop.flex.order.val
+        //     obj[index] = prop
+        //     length++
+        //   })
+        //   obj[0].flex.setKey('order', length)
+        //   for (var i = 1; i <= length; i++) {
+        //     obj[i].flex.setKey('order', i - 1)
+        //   }
+        //   this.setKey('marginLeft', cnt)
+        // }, 300)
       }
     }
   },
@@ -87,21 +78,21 @@ app.set({
     html: 'right',
     on: {
       click (e, event) {
-        app.carousel.items.setKey('animateLeft', --cnt)
-        app.carousel.items.postpone('carousel', function () {
-          var obj = {}
-          var length = -1
-          this.each(function (prop, key) {
-            var index = prop.flex.order.val
-            obj[index] = prop
-            length++
-          })
-          obj[length].flex.setKey('order', 0)
-          for (var i = 0; i < length; i++) {
-            obj[i].flex.setKey('order', i + 1)
-          }
-          this.setKey('marginLeft', cnt)
-        }, 300)
+        // app.carousel.items.setKey('animateLeft', --cnt)
+        // app.carousel.items.postpone('carousel', function () {
+        //   var obj = {}
+        //   var length = -1
+        //   this.each(function (prop, key) {
+        //     var index = prop.flex.order.val
+        //     obj[index] = prop
+        //     length++
+        //   })
+        //   obj[length].flex.setKey('order', 0)
+        //   for (var i = 0; i < length; i++) {
+        //     obj[i].flex.setKey('order', i + 1)
+        //   }
+        //   this.setKey('marginLeft', cnt)
+        // }, 300)
       }
     }
   }

758b445a96d8f22f4d7012978ad814a69eaf16a7
Jim de Beer <jim@vigour.io>
2016-02-04 12:15:38 +0100

    added styling

--- example/refactor/todo.js
+++ example/refactor/todo.js
@@ -16,82 +16,74 @@ var app = new Element({
   DOM: document.body
 })
 
-// var Todo = new Element({
-//   type: 'li',
-//   view: {
-//     toggle: {
-//       type: 'input',
-//       attributes: {
-//         type: 'checkbox'
-//       }
-//     },
-//     title: {
-//       type: 'label',
-//       text: { $: 'title' }
-//     },
-//     destroy: {
-//       type: 'button',
-//       on: {
-//         down () {
-//           console.log('ok remove!')
-//           app.patch()
-//         }
-//       }
-//     }
-//   },
-//   edit: {
-//     type: 'input'
-//   }
-// }).Constructor
+var Todo = new Element({
+  type: 'li',
+  view: {
+    toggle: {
+      type: 'input',
+      attributes: {
+        type: 'checkbox'
+      }
+    },
+    title: {
+      type: 'label',
+      text: { $: 'title' }
+    },
+    destroy: {
+      type: 'button',
+      on: {
+        down () {
+          console.log('ok remove!')
+          app.patch()
+        }
+      }
+    }
+  },
+  edit: {
+    type: 'input'
+  }
+}).Constructor
 
 app.set({
-  bla: {
-    text: 'bla',
+  todoapp: {
     on: {
-      mousedown () {
-        console.log('1')
+      click () {
+        console.log('?')
+      }
+    },
+    header: {
+      type: 'header',
+      title: {
+        type: 'h1',
+        text: 'todo-app'
+      },
+      ['new-todo']: {
+        type: 'input',
+        attributes: {
+          placeholder: 'What needs to be done?'
+        },
+        on: {
+          keyup () {
+            console.log('lullllz')
+          }
+        }
+      },
+      main: {
+        type: 'section',
+        ['toggle-all']: {
+          type: 'input',
+          attributes: {
+            type: 'checkbox',
+            checked: true
+          }
+        },
+        ['todo-list']: {
+          type: 'ul',
+          $collection: true,
+          Child: Todo,
+          val: todos
+        }
       }
     }
   }
-  // todoapp: {
-  //   on: {
-  //     click () {
-  //       console.log('?')
-  //     }
-  //   },
-  //   header: {
-  //     type: 'header',
-  //     title: {
-  //       type: 'h1',
-  //       text: 'todo-app'
-  //     },
-  //     ['new-todo']: {
-  //       type: 'input',
-  //       attributes: {
-  //         placeholder: 'What needs to be done?'
-  //       },
-  //       on: {
-  //         keyup () {
-  //           console.log('lullllz')
-  //         }
-  //       }
-  //     },
-  //     main: {
-  //       type: 'section',
-  //       ['toggle-all']: {
-  //         type: 'input',
-  //         attributes: {
-  //           type: 'checkbox',
-  //           checked: true
-  //         }
-  //       },
-  //       ['todo-list']: {
-  //         type: 'ul',
-  //         $collection: true,
-  //         Child: Todo,
-  //         val: todos
-  //       }
-  //     }
-  //   }
-  // }
 })

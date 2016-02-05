11a9bbad399700384b3026d42d65417d94188c54
Jim de Beer <jim@vigour.io>
2016-02-05 13:08:10 +0100

    keys

--- example/refactor/todo.js
+++ example/refactor/todo.js
@@ -2,30 +2,40 @@
 require('./todo.less')
 var Element = require('../../lib')
 var Observable = require('vigour-js/lib/observable')
-Observable.prototype.inject(require('../../lib/subscription/stamp'))
-
+// Observable.prototype.inject(require('=../../lib/subscription/stamp'))
 // ----- data ----
-var Syncable = require('vigour-hub/lib/syncable')
-Syncable.prototype.inject(require('../../lib/subscription/stamp'))
-var Hub = require('vigour-hub')
-var hub = global.hub = new Hub({
-  adapter: {
-    inject: require('vigour-hub/lib/protocol/websocket'),
-    websocket: 'ws://localhost:3033',
-    scope: {
-      val: 'james',
-      inject: require('vigour-js/lib/observable/storage')
-    }
+// var Syncable = require('vigour-hub/lib/syncable')
+// Syncable.prototype.inject(require('../../lib/subscription/stamp'))
+// var Hub = require('vigour-hub')
+// var hub = global.hub = new Hub({
+//   adapter: {
+//     inject: require('vigour-hub/lib/protocol/websocket'),
+//     websocket: 'ws://localhost:3033',
+//     scope: {
+//       val: 'james',
+//       inject: require('vigour-js/lib/observable/storage')
+//     }
+//   }
+// })
+// var todos = hub.get('shows', {})
+
+var todos = new Observable({
+  inject: require('../../lib/subscription/stamp'),
+  // Child: 'Constructor'
+})
+
+todos.set({
+  bla: {
+    title: 'hello'
   }
 })
-var todos = hub.get('shows', {})
 
-// ----- ui -----
+// // ----- ui -----
 var app = global.app = new Element({
   DOM: document.body
 })
 
-// ----- todo -----
+// // ----- todo -----
 var Todo = new Element({
   type: 'li',
   view: {
@@ -68,7 +78,7 @@ var Todo = new Element({
   }
 }).Constructor
 
-// ----- app -----
+// // ----- app -----
 app.set({
   time: {
     text: {}
@@ -80,29 +90,30 @@ app.set({
         type: 'h1',
         text: 'todo-app'
       },
-      user: {
-        type: 'input',
-        css: 'new-todo',
-        value: hub.adapter.scope,
-        on: {
-          keyup (e, event) {
-            hub.adapter.scope.set(e.currentTarget.value, event)
-          }
-        }
-      },
+      // user: {
+      //   type: 'input',
+      //   css: 'new-todo',
+      //   value: hub.adapter.scope,
+      //   on: {
+      //     keyup (e, event) {
+      //       hub.adapter.scope.set(e.currentTarget.value, event)
+      //     }
+      //   }
+      // },
       ['new-todo']: {
         type: 'input',
         attributes: {
           placeholder: {
-            val: hub.adapter.scope,
-            $add: ', what needs to be done?'
+            // val: hub.adapter.scope,
+            val: ', what needs to be done?'
             // support operator is now broken -- also for cases!
           }
         },
         on: {
           keydown (e, event) {
             if (e.keyCode === 13) {
-              todos.set({ [hub.adapter.scope.val + '' + Math.random() * 9999]: {
+              //hub.adapter.scope.val
+              todos.set({ [ ('z-' + Math.random() * 9999) ]: {
                 title: e.currentTarget.value || 'new todo' }
               }, event)
               e.currentTarget.value = ''

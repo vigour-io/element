6a429fcf6550a205451d9940ba55514681b012ab
Youri Daamen <me@youz.io>
2016-02-10 10:28:33 +0100

    ref example player and switcher debugging

--- example/switcher/index.js
+++ example/switcher/index.js
@@ -3,6 +3,13 @@ require('./style.less')
 
 var Switcher = require('../../lib/switcher')
 var Observable = require('vigour-js/lib/observable')
+
+Observable = new Observable().Constructor
+Observable.prototype.inject(require('../../lib/subscription/stamp'))
+Observable.prototype.set({
+  Child: Observable
+})
+
 var Element = require('../../lib')
 
 var app = global.app = new Element({
@@ -10,6 +17,7 @@ var app = global.app = new Element({
 })
 
 var switcher = new Switcher({
+  $: true,
   w: 300,
   h: 200,
   config: {
@@ -19,33 +27,37 @@ var switcher = new Switcher({
       duration: 300
     }
   },
-  ChildConstructor: new Element({
+  Child: {
     $: true,
     html: {
       $: 'title'
     }
-  }),
+  },
   next: {
     $: 'current'
   }
 })
 
 var data = new Observable({
-  current: {}
+  current: new Observable({
+    key: 'nerdje',
+    title: '?????'
+  })
 })
 
 global.app = app.set({
   holder: {
     switcher: new switcher.Constructor(data)
-  },
+  }
   // holder2: {
   //   switcher: new switcher.Constructor()
   // }
 })
 
-data.current.set(new Observable({
-  title: 'heeee'
-}))
+// data.current.set(new Observable({
+//   key: 'durt',
+//   title: 'heeee'
+// }))
 
 // setInterval(function () {
 //   data.current.val = new Observable({
@@ -57,14 +69,19 @@ data.current.set(new Observable({
 var cnt = 1
 var id = setInterval(function () {
   let key = Math.random()
-  app.holder.switcher.set({[key]: new Observable({
-    key: key,
+  // app.holder.switcher.set({[key]: new Observable({
+  //   key: key,
+  //   title: key
+  // })})
+  console.error('!!')
+  // data.current.set(new Observable({
+  //   key: 'durt',
+  //   title: 'heeee'
+  // }))
+  data.current.set(new Observable({
+    key: '' + key,
     title: key
-  })})
-    // data.current.set(new Observable({
-    //   key: key,
-    //   title: key
-    // }))
+  }))
     // app.holder.switcher.set({[key]: {
     //   html: 'flups:' + key
     // }})

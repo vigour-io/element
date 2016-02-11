0a3bfa9904f865a4ff6e5a8af0331f1d724fc856
Jim de Beer <jim@vigour.io>
2016-02-11 18:03:09 +0100

    Child cntr

--- example/play/components/text/index.js
+++ example/play/components/text/index.js
@@ -1,6 +1,9 @@
 'use strict'
 
 exports.title = {
-  type: 'h1',
-  text: 'this is a title'
+  // type: 'h1',
+  nest: {
+    text: 'bla'
+  },
+  text: 'this is a title' // { $: 'title' }
 }
--- example/play/index.js
+++ example/play/index.js
@@ -4,12 +4,12 @@ var e = require('../../e')
 
 var merge = require('lodash/object/merge')
 var components = {}
-merge(components, require('./components/img'))
-merge(components, require('./components/item'))
-merge(components, require('./components/list'))
+// merge(components, require('./components/img'))
+// merge(components, require('./components/item'))
+// merge(components, require('./components/list'))
 merge(components, require('./components/text'))
 
-var app = global.app = e({
+var app = global.app = e([{
   key: 'app',
   components: components,
   Child: {
@@ -20,15 +20,30 @@ var app = global.app = e({
       }
     }
   },
-  images: {
-    thumb: { type: 'thumb' },
-    poster: { type: 'poster' },
-    img: { type: 'img' }
-  },
-  items: {
+  mybitch: {
 
   },
+  // images: {
+  //   thumb: { type: 'thumb' },
+  //   poster: { type: 'poster' },
+  //   img: { type: 'img' }
+  // },
+  // items: {
+  //   showtje: {
+  //     title: 'sjowtje'
+  //   }
+  // },
   DOM: document.body
-})
+}, {
+
+  // components: {
+  //   thumb: {
+  //     src: { $add: '/gideon.flups' }
+  //   }
+  // }
+
+}])
+
+console.error(app.Child.prototype._title, !!app.Child.prototype._title._input)
 
 app.render()

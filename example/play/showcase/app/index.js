fab96f6f538ba56070ace7996e0976f72ebcd9c2
Jim de Beer <jim@vigour.io>
2016-02-13 16:09:09 +0100

    removed old player

--- example/play/showcase/app/index.js
+++ example/play/showcase/app/index.js
@@ -1,7 +1,8 @@
 'use strict'
-require('./lists.js')
+// require('./lists.js')
 // require('./pages.js')
 console.clear()
 // require('./debug.js')
 // require('../refactor/todo')
+
 require('./switcher')

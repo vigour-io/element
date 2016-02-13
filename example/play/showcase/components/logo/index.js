b063c485b181888401dffa728a729004c491fef8
nikoloza <nika.tomadze@gmail.com>
2016-02-13 23:57:44 +0100

    sidebar

---
+++ example/play/showcase/components/logo/index.js
@@ -0,0 +1,8 @@
+'use strict'
+
+exports.logo = {
+  icon: {
+    type: 'icon',
+    css: 'icon-logo'
+  }
+}
---
+++ example/play/showcase/components/sidebar/index.js
@@ -0,0 +1,38 @@
+'use strict'
+
+exports.sidebar = {
+  logo: {
+    type: 'logo'
+  },
+
+  menu: {
+    // Child: {
+    // }
+    div: require('../../app/buttons')
+  },
+
+  meta: {
+    links: {
+
+    },
+    user: {
+      buttons: {
+
+      },
+      info: {
+        profile: {
+
+        },
+
+        icons: {
+          settings: {
+
+          },
+          cast: {
+
+          }
+        }
+      }
+    }
+  }
+}

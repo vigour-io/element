0043319073e170d78c07f104f580baa3a859aa05
Gideon van Dijk <gideon.vandijk@uxmade.com>
2016-02-14 18:56:07 +0100

    added duration

--- example/play/showcase/raw/publishers.js
+++ example/play/showcase/raw/publishers.js
@@ -2,8 +2,8 @@ module.exports = {
 	title: 'Publishers',
 	icon: 'publishers',
 	items: {
-		focus: 0,
-    length: 2,
+		// focus: 0,
+    // length: 3,
 	  0: {
 	    title: 'Dubai Lynx',
 	    description: 'Description of the Publisher',
@@ -13,40 +13,71 @@ module.exports = {
         poster: 'http://share.vigour.io/401H2C3e2b24/poster.png'
       },
       items: {
-      	focus: 0,
-	      length: 3,
-	      0: {
+      	// focus: 0,
+	      // length: 2,
+	      '0:videos': {
 	        title: 'Feautured on Dubai Lynx',
-	        link: [ '$', 'publishers', 'items', 0, 'items', 'channels' ], // order is irrelevant now
-	        items: [
-	          [ '$', 'channels', 'items', '0' ],
-	          [ '$', 'channels', 'items', '1' ],
-	          [ '$', 'channels', 'items', '2' ],
-	          [ '$', 'channels', 'items', '3' ],
-	          [ '$', 'channels', 'items', '4' ]
-	        ]
+	        link: [ '$', 'publishers', 'items', 0, 'items', 'videos' ], 
+	        items: {
+            // focus: 0,
+            // length: 2,
+            0: {
+              title: 'Video Title',
+              description: 'Description of this Video',
+              img: {
+                thumb: 'http://share.vigour.io/2G2G003J3A39/thumb.png',
+                spotlight: 'http://share.vigour.io/152i2U0C1J3B/spotlight.png',
+                poster: 'http://share.vigour.io/401H2C3e2b24/poster.png'
+              },
+              video: '/api/mena-web/asset/3787/play',
+              time: 0.3,
+              duration: 201 * 60 * 1e3
+            },
+            1: {
+              title: 'Episode Title',
+              description: 'Description of this Video',
+              img: {
+                thumb: 'http://share.vigour.io/2G2G003J3A39/thumb.png',
+                spotlight: 'http://share.vigour.io/152i2U0C1J3B/spotlight.png',
+                poster: 'http://share.vigour.io/401H2C3e2b24/poster.png'
+              },
+              video: '/api/mena-web/asset/3787/play',
+              time: 0.3,
+              duration: 201 * 60 * 1e3
+            }
+          }
 	      },
-	      1: {
-	        title: 'Lynx Drama',
-	        link: [ '$', 'channels' ], // order is irrelevant now
-	        items: [
-	          [ '$', 'channels', 'items', '0' ],
-	          [ '$', 'channels', 'items', '1' ],
-	          [ '$', 'channels', 'items', '2' ],
-	          [ '$', 'channels', 'items', '3' ],
-	          [ '$', 'channels', 'items', '4' ]
-	        ]
-	      },
-	      2: {
-	        title: 'Lynx Movies',
-	        link: [ '$', 'channels' ], // order is irrelevant now
-	        items: [
-	          [ '$', 'channels', 'items', '0' ],
-	          [ '$', 'channels', 'items', '1' ],
-	          [ '$', 'channels', 'items', '2' ],
-	          [ '$', 'channels', 'items', '3' ],
-	          [ '$', 'channels', 'items', '4' ]
-	        ]
+	      '1:videos': {
+	        title: 'Latest Videos',
+	        link: [ '$', 'channels' ], 
+	        items: {
+            // focus: 0,
+            // length: 2,
+            0: {
+              title: 'Video Title',
+              description: 'Description of this Video',
+              img: {
+                thumb: 'http://share.vigour.io/2G2G003J3A39/thumb.png',
+                spotlight: 'http://share.vigour.io/152i2U0C1J3B/spotlight.png',
+                poster: 'http://share.vigour.io/401H2C3e2b24/poster.png'
+              },
+              video: '/api/mena-web/asset/3787/play',
+              time: 0.3,
+              duration: 201 * 60 * 1e3
+            },
+            1: {
+              title: 'Episode Title',
+              description: 'Description of this Video',
+              img: {
+                thumb: 'http://share.vigour.io/2G2G003J3A39/thumb.png',
+                spotlight: 'http://share.vigour.io/152i2U0C1J3B/spotlight.png',
+                poster: 'http://share.vigour.io/401H2C3e2b24/poster.png'
+              },
+              video: '/api/mena-web/asset/3787/play',
+              time: 0.3,
+              duration: 201 * 60 * 1e3
+            }
+          }
 	      }
     	}
 	  },
@@ -59,40 +90,148 @@ module.exports = {
         poster: 'http://share.vigour.io/401H2C3e2b24/poster.png'
       },
       items: {
-      	focus: 0,
-      	length: 3,
-	      0: {
+      	// focus: 0,
+	      // length: 2,
+	      '0:videos': {
 	        title: 'Feautured on The Toolshop',
-	        link: [ '$', 'channels' ], // order is irrelevant now
-	        items: [
-	          [ '$', 'channels', 'items', '0' ],
-	          [ '$', 'channels', 'items', '1' ],
-	          [ '$', 'channels', 'items', '2' ],
-	          [ '$', 'channels', 'items', '3' ],
-	          [ '$', 'channels', 'items', '4' ]
-	        ]
+	        link: [ '$', 'publishers', 'items', 0, 'items', 'channels' ], 
+	        items: {
+            // focus: 0,
+            // length: 2,
+            0: {
+              title: 'Video Title',
+              description: 'Description of this Video',
+              img: {
+                thumb: 'http://share.vigour.io/2G2G003J3A39/thumb.png',
+                spotlight: 'http://share.vigour.io/152i2U0C1J3B/spotlight.png',
+                poster: 'http://share.vigour.io/401H2C3e2b24/poster.png'
+              },
+              video: '/api/mena-web/asset/3787/play',
+              time: 0.3,
+              duration: 201 * 60 * 1e3
+            },
+            1: {
+              title: 'Episode Title',
+              description: 'Description of this Video',
+              img: {
+                thumb: 'http://share.vigour.io/2G2G003J3A39/thumb.png',
+                spotlight: 'http://share.vigour.io/152i2U0C1J3B/spotlight.png',
+                poster: 'http://share.vigour.io/401H2C3e2b24/poster.png'
+              },
+              video: '/api/mena-web/asset/3787/play',
+              time: 0.3,
+              duration: 201 * 60 * 1e3
+            }
+          }
 	      },
-	      1: {
+	      '1:videos': {
 	        title: 'Latest Videos',
-	        link: [ '$', 'channels' ], // order is irrelevant now
-	        items: [
-	          [ '$', 'channels', 'items', '0' ],
-	          [ '$', 'channels', 'items', '1' ],
-	          [ '$', 'channels', 'items', '2' ],
-	          [ '$', 'channels', 'items', '3' ],
-	          [ '$', 'channels', 'items', '4' ]
-	        ]
+	        link: [ '$', 'channels' ], 
+	        items: {
+            // focus: 0,
+            // length: 2,
+            0: {
+              title: 'Video Title',
+              description: 'Description of this Video',
+              img: {
+                thumb: 'http://share.vigour.io/2G2G003J3A39/thumb.png',
+                spotlight: 'http://share.vigour.io/152i2U0C1J3B/spotlight.png',
+                poster: 'http://share.vigour.io/401H2C3e2b24/poster.png'
+              },
+              video: '/api/mena-web/asset/3787/play',
+              time: 0.3,
+              duration: 201 * 60 * 1e3
+            },
+            1: {
+              title: 'Episode Title',
+              description: 'Description of this Video',
+              img: {
+                thumb: 'http://share.vigour.io/2G2G003J3A39/thumb.png',
+                spotlight: 'http://share.vigour.io/152i2U0C1J3B/spotlight.png',
+                poster: 'http://share.vigour.io/401H2C3e2b24/poster.png'
+              },
+              video: '/api/mena-web/asset/3787/play',
+              time: 0.3,
+              duration: 201 * 60 * 1e3
+            }
+          }
+	      }
+    	}
+	  },
+	  2: {
+	    title: 'Guild Of Dubai',
+	    description: 'Description of the Publisher',
+	    img: {
+        thumb: 'http://share.vigour.io/2G2G003J3A39/thumb.png',
+        spotlight: 'http://share.vigour.io/152i2U0C1J3B/spotlight.png',
+        poster: 'http://share.vigour.io/401H2C3e2b24/poster.png'
+      },
+      items: {
+      	// focus: 0,
+	      // length: 2,
+	      '0:posters': {
+	        title: 'Feautured on Guild Of',
+	        link: [ '$', 'publishers', 'items', 0, 'items', 'channels' ], 
+	        items: {
+            // focus: 0,
+            // length: 2,
+            0: {
+              title: 'Video Title',
+              description: 'Description of this Video',
+              img: {
+                thumb: 'http://share.vigour.io/2G2G003J3A39/thumb.png',
+                spotlight: 'http://share.vigour.io/152i2U0C1J3B/spotlight.png',
+                poster: 'http://share.vigour.io/401H2C3e2b24/poster.png'
+              },
+              video: '/api/mena-web/asset/3787/play',
+              time: 0.3,
+              duration: 201 * 60 * 1e3
+            },
+            1: {
+              title: 'Episode Title',
+              description: 'Description of this Video',
+              img: {
+                thumb: 'http://share.vigour.io/2G2G003J3A39/thumb.png',
+                spotlight: 'http://share.vigour.io/152i2U0C1J3B/spotlight.png',
+                poster: 'http://share.vigour.io/401H2C3e2b24/poster.png'
+              },
+              video: '/api/mena-web/asset/3787/play',
+              time: 0.3,
+              duration: 201 * 60 * 1e3
+            }
+          }
 	      },
-	      2: {
-	        title: 'Building a Metal Lathe',
-	        link: [ '$', 'channels' ], // order is irrelevant now
-	        items: [
-	          [ '$', 'channels', 'items', '0' ],
-	          [ '$', 'channels', 'items', '1' ],
-	          [ '$', 'channels', 'items', '2' ],
-	          [ '$', 'channels', 'items', '3' ],
-	          [ '$', 'channels', 'items', '4' ]
-	        ]
+	      '1:posters': {
+	        title: 'Latest Videos',
+	        link: [ '$', 'channels' ], 
+	        items: {
+            // focus: 0,
+            // length: 2,
+            0: {
+              title: 'Video Title',
+              description: 'Description of this Video',
+              img: {
+                thumb: 'http://share.vigour.io/2G2G003J3A39/thumb.png',
+                spotlight: 'http://share.vigour.io/152i2U0C1J3B/spotlight.png',
+                poster: 'http://share.vigour.io/401H2C3e2b24/poster.png'
+              },
+              video: '/api/mena-web/asset/3787/play',
+              time: 0.3,
+              duration: 201 * 60 * 1e3
+            },
+            1: {
+              title: 'Episode Title',
+              description: 'Description of this Video',
+              img: {
+                thumb: 'http://share.vigour.io/2G2G003J3A39/thumb.png',
+                spotlight: 'http://share.vigour.io/152i2U0C1J3B/spotlight.png',
+                poster: 'http://share.vigour.io/401H2C3e2b24/poster.png'
+              },
+              video: '/api/mena-web/asset/3787/play',
+              time: 0.3,
+              duration: 201 * 60 * 1e3
+            }
+          }
 	      }
     	}
 	  }

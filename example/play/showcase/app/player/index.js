d95dd07ea51addf68845835ee2f3cff31d961bbf
Youri Daamen <me@youz.io>
2016-02-15 01:55:48 +0100

    player config removed and added guard for src

--- example/play/showcase/app/player/index.js
+++ example/play/showcase/app/player/index.js
@@ -3,38 +3,47 @@ var Player = require('../../../../../lib/player/')
 var videoAPi = require('../api/video')
 var playback = videoAPi.getSource
 
-var orRender = Player.src.render
 Player.src.define({
-  render () {
-    var onRenderArgs = arguments
-    var store = this.storeContext()
-    playback.on('response', (data) => {
-      var videoUrls = data.videoUrl
-      if (videoUrls.length > 1) {
-        let source = {}
-        for (let i = videoUrls.length - 1; i >= 0; i--) {
-          let url = videoUrls[i]
-          let ext = url.substring(url.lastIndexOf('.') + 1)
-          if (ext === 'mpd') {
-            source.dash = url
-          } else {
-            source.hls = url
+  render (val) {
+    if (val) {
+      var store = this.storeContext()
+      playback.on('response', (data) => {
+        var videoUrls = data.videoUrl
+        if (videoUrls.length > 1) {
+          let source = {}
+          for (let i = videoUrls.length - 1; i >= 0; i--) {
+            let url = videoUrls[i]
+            let ext = url.substring(url.lastIndexOf('.') + 1)
+            if (ext === 'mpd') {
+              source.dash = url
+            } else {
+              source.hls = url
+            }
           }
+          val = source
+        } else if (videoUrls.length > 0) {
+          val = videoUrls[0]
         }
-        onRenderArgs[0] = source
-      } else if (videoUrls.length > 0) {
-        onRenderArgs[0] = videoUrls[0]
-      }
-      this.applyContext(store)
-      orRender.apply(this, onRenderArgs)
-    })
-    playback.on('error', (err) => {
-      console.error(err)
-      this.emit('error', err)
-    })
-    playback.set({
-      asset: this.val,
-      protocol: 'HLS,DASH'
-    })
+
+        this.applyContext(store)
+        var player = this.parent
+        if (player.initialised && player.instance) {
+          player.ready.origin.set(false)
+          player.ready.render(false)
+          player.load(val)
+        } else {
+          player.init(val)
+        }
+      })
+
+      playback.on('error', (err) => {
+        this.emit('error', err)
+      })
+      playback.set({
+        asset: val,
+        protocol: 'HLS,DASH'
+      }, false)
+      playback.asset.emit('data')
+    }
   }
 })

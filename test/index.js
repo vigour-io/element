663bdc06f4e109fee1f0cf3feff88783c5e0ef9c
Jim de Beer <jim@vigour.io>
2016-02-23 20:18:14 +0100

    html generator tests

--- test/index.js
+++ test/index.js
@@ -8,7 +8,23 @@ var fakeDom = {}
 
 // do this with a conditional require or something
 var app = e({
+  bla: {
+    text: 'geinig!',
+    gurk: {
+      text: 'bla'
+    }
+  },
   DOM: fakeDom
 })
 
 console.log('gurk', app.renderTree)
+var toHTML = require('vdom-to-html')
+
+var http = require('http')
+
+var server = http.createServer((req,res) => {
+  res.setHeader('Content-Type', 'text/html')
+  res.writeHead(200, {'Content-Type': 'text/html'})
+  var str = '<html><head></head><body>' + toHTML(app.renderTree) + '</body></html>'
+  res.end(str)
+}).listen(3031)

be5be84a29eae40bdfb9558bbfb8371d911c6aeb
Jim de Beer <jim@vigour.io>
2016-03-01 15:30:39 +0100

    added test for observable types, added straigh string functionality

--- test/index.js
+++ test/index.js
@@ -20,23 +20,9 @@ test('children and text', function (t) {
   )
 })
 
-test('complex observable types', function (t) {
+test('complex observable types and string in element', function (t) {
+  t.plan(1)
   var Observable = require('vigour-observable')
-  var Obs = new Observable({
-    define: {
-      handleContextChild (data, event, context) {
-        this.clearContext()
-        this.emit('data', data, event)
-      }
-    },
-    on: {
-      data () {
-        console.log('fire it!')
-      }
-    },
-    Child: 'Constructor'
-  }).Constructor
-
   var injectable = {
     api: {
       type: 'observable',
@@ -44,9 +30,7 @@ test('complex observable types', function (t) {
         api: {
           on: {
             data: {
-              api () {
-                console.error('yo api!', this.key, this.origin.path)
-              }
+              api () {}
             }
           }
         }
@@ -55,145 +39,143 @@ test('complex observable types', function (t) {
       language: {}
     }
   }
-
   var elem = require('../').prototype
   elem.inject(injectable)
-
   var app = e({
     child: 'hello',
     DOM: fakeDom
   })
   var output = toHTML(app.renderTree)
-  console.log(output)
+  t.equal(output, '<div><div class="child">hello</div></div>')
 })
 
-// test('attributes, types and data', function (t) {
-//   t.plan(1)
-//   var Observable = require('vigour-observable')
-//   // will make this into a seperate module
-//   var Data = new Observable({
-//     inject: require('vigour-observable/lib/data')
-//   }).Constructor
-//   var app = e({
-//     components: {
-//       checkbox: {
-//         type: 'input',
-//         attributes: {
-//           type: { $: 'node-type' },
-//           checked: {
-//             $transform () {
-//               return true
-//             }
-//           }
-//         }
-//       }
-//     },
-//     child: { type: 'checkbox' },
-//     val: new Data({ 'node-type': 'checkbox' }),
-//     DOM: fakeDom
-//   })
+test('attributes, types and data', function (t) {
+  t.plan(1)
+  var Observable = require('vigour-observable')
+  // will make this into a seperate module
+  var Data = new Observable({
+    inject: require('vigour-observable/lib/data')
+  }).Constructor
+  var app = e({
+    components: {
+      checkbox: {
+        type: 'input',
+        attributes: {
+          type: { $: 'node-type' },
+          checked: {
+            $transform () {
+              return true
+            }
+          }
+        }
+      }
+    },
+    child: { type: 'checkbox' },
+    val: new Data({ 'node-type': 'checkbox' }),
+    DOM: fakeDom
+  })
 
-//   process.nextTick(function () {
-//     var output = toHTML(app.renderTree)
-//     t.equal(
-//       output,
-//       '<div><input type="checkbox" checked class="child type-checkbox"></div>'
-//     )
-//   })
-// })
+  process.nextTick(function () {
+    var output = toHTML(app.renderTree)
+    t.equal(
+      output,
+      '<div><input type="checkbox" checked class="child type-checkbox"></div>'
+    )
+  })
+})
 
-// test('css compare functionality with complex types', function (t) {
-//   t.plan(1)
-//   var app = e({
-//     components: {
-//       a1: {
-//         text: 'hello!',
-//         css: { s: 'i-a1' }
-//       },
-//       b1: {
-//         css: { inherits: 'i-b1' },
-//         a1: { type: 'a1' }
-//       },
-//       b2: {
-//         type: 'b1',
-//         css: { inherits: 'i-b2' },
-//       },
-//       p1: { s: { type: 'b1' } },
-//       p2: { s: { type: 'b2' } }
-//     },
-//     posts: { type: 'p1' },
-//     DOM: fakeDom
-//   })
-//   app.set({ posts: { type: 'p2' }})
-//   process.nextTick(function () {
-//     var output = toHTML(app.renderTree)
-//     t.equal(
-//       output,
-//       '<div><div class="posts type-p2"><div class="type-b2 s i-b2"><div class="type-a1 a1 i-a1">hello!</div></div></div></div>'
-//     )
-//   })
-// })
+test('css compare functionality with complex types', function (t) {
+  t.plan(1)
+  var app = e({
+    components: {
+      a1: {
+        text: 'hello!',
+        css: { s: 'i-a1' }
+      },
+      b1: {
+        css: { inherits: 'i-b1' },
+        a1: { type: 'a1' }
+      },
+      b2: {
+        type: 'b1',
+        css: { inherits: 'i-b2' },
+      },
+      p1: { s: { type: 'b1' } },
+      p2: { s: { type: 'b2' } }
+    },
+    posts: { type: 'p1' },
+    DOM: fakeDom
+  })
+  app.set({ posts: { type: 'p2' }})
+  process.nextTick(function () {
+    var output = toHTML(app.renderTree)
+    t.equal(
+      output,
+      '<div><div class="posts type-p2"><div class="type-b2 s i-b2"><div class="type-a1 a1 i-a1">hello!</div></div></div></div>'
+    )
+  })
+})
 
-// test('creating and using cases in element', function (t) {
-//   t.plan(2)
-//   // cases have to be scoped to an app not global on elem, it's annoying to test!
-//   var app = e({
-//     cases: { $test: true },
-//     text: 'nothing',
-//     child: { text: 'child' },
-//     $test: {
-//       text: 'active',
-//       child: {
-//         text: {
-//           val: [ '$', 'cases', '$test' ],
-//           $transform (val) {
-//             if (val === true) {
-//               return '$test'
-//             }
-//           }
-//         }
-//       }
-//     },
-//     DOM: fakeDom
-//   })
-//   var output = toHTML(app.renderTree)
-//   t.equal(output, '<div>nothing<div class="child">$test</div>active</div>')
-//   app.cases.$test.val = false
-//   process.nextTick(function () {
-//     output = toHTML(app.renderTree)
-//     t.equal(output, '<div><div class="child">child</div>nothing</div>')
-//   })
-// })
+test('creating and using cases in element', function (t) {
+  t.plan(2)
+  // cases have to be scoped to an app not global on elem, it's annoying to test!
+  var app = e({
+    cases: { $test: true },
+    text: 'nothing',
+    child: { text: 'child' },
+    $test: {
+      text: 'active',
+      child: {
+        text: {
+          val: [ '$', 'cases', '$test' ],
+          $transform (val) {
+            if (val === true) {
+              return '$test'
+            }
+          }
+        }
+      }
+    },
+    DOM: fakeDom
+  })
+  var output = toHTML(app.renderTree)
+  t.equal(output, '<div>nothing<div class="child">$test</div>active</div>')
+  app.cases.$test.val = false
+  process.nextTick(function () {
+    output = toHTML(app.renderTree)
+    t.equal(output, '<div><div class="child">child</div>nothing</div>')
+  })
+})
 
-// test('conditional subscription', function (t) {
-//   t.plan(2)
-//   var Observable = require('vigour-observable')
-//   var Data = new Observable({
-//     inject: require('vigour-observable/lib/data')
-//   }).Constructor
-//   var app = e({
-//     components: {
-//       title: {
-//         $: {
-//           val: true,
-//           condition (data, event) {
-//             return data && data.loaded && data.loaded.val
-//           }
-//         },
-//         text: { $: 'title' }
-//       }
-//     },
-//     DOM: fakeDom,
-//     title: { type: 'title' },
-//     val: new Data({
-//       title: 'title'
-//     })
-//   })
-//   var output = toHTML(app.renderTree)
-//   t.equal(output, '<div></div>')
-//   app.origin.set({ loaded: true })
-//   process.nextTick(function () {
-//     output = toHTML(app.renderTree)
-//     t.equal(output, '<div><div class="title type-title">title</div></div>')
-//   })
-// })
+test('conditional subscription', function (t) {
+  t.plan(2)
+  var Observable = require('vigour-observable')
+  var Data = new Observable({
+    inject: require('vigour-observable/lib/data')
+  }).Constructor
+  var app = e({
+    components: {
+      title: {
+        $: {
+          val: true,
+          condition (data, event) {
+            return data && data.loaded && data.loaded.val
+          }
+        },
+        text: { $: 'title' }
+      }
+    },
+    DOM: fakeDom,
+    title: { type: 'title' },
+    val: new Data({
+      title: 'title'
+    })
+  })
+  var output = toHTML(app.renderTree)
+  t.equal(output, '<div></div>')
+  app.origin.set({ loaded: true })
+  process.nextTick(function () {
+    output = toHTML(app.renderTree)
+    t.equal(output, '<div><div class="title type-title">title</div></div>')
+  })
+})

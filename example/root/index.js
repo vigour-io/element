9a8899ce3d3b5e2de2da0d46f6d0dcd8cb3eea16
Jim de Beer <jim@vigour.io>
2016-04-28 20:03:29 +0200

    start of root -- gaurds

--- example/root/index.js
+++ example/root/index.js
@@ -4,6 +4,35 @@ require('../style.css')
 const State = require('vigour-state')
 const Element = require('../../lib/element')
 const render = require('../../lib/render')
-const s = new State()
-const app = new Element({ text: 'hello' })
-document.body.appendChild(render(app, s))
+const s = new State({
+  a: {
+    b: 'its a.b'
+  },
+  c: {
+    d: {
+      e: 'e'
+    }
+  },
+  b: 'its root.b'
+})
+
+const app = new Element({
+  text: 'hello',
+  a: {
+    // $: 'a',
+    b: {
+      // text: { $: 'c.d.e' }
+      // something extra funky going on with root...
+      text: { $: '$root.b' }
+      // $: 'b.$root.b'
+    }
+  }
+})
+
+// const app = new Element({ text: 'hello' })
+document.body.appendChild(render(app, s, 'dom', function (type, stamp, subs, tree) {
+  console.log('yo yo yo', this.path(), type, stamp, tree)
+  // 2 times a never root?
+}))
+
+s.c.d.e.set('hello its e!')

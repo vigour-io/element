4c036cc0122ecfd0c06180b5d93e92363c55d983
Jim de Beer <jim@vigour.io>
2016-04-28 12:44:13 +0200

    trying budo + post css

--- example/run.js
+++ example/run.js
@@ -2,7 +2,11 @@
 // "budo": "budo -p 8080 --css ./bundle.css --live -- -r ./package.json:package.json -g [ babelify --presets [ es2015 ] ]",
 const spawn = require('child_process').spawn
 const budo = spawn('budo', [
-  './example/collection.js', '-p 8080 --css ./bundle.css --live -- -r ./package.json:package.json -g [ babelify --presets [ es2015 ] ]'
+  './example/collection.js',
+  `-p 8080
+  --css ./example/bundles/bundle.css
+  --live -- -r ./package.json:package.json
+  -g [ babelify --presets [ es2015 ] ]`
 ])
 
 console.log(process.argv)

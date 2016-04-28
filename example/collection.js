634551a98519c2d5d933ca9675255faf825670b4
Jim de Beer <jim@vigour.io>
2016-04-28 12:02:03 +0200

    css vs style

--- example/collection.js
+++ example/collection.js
@@ -5,7 +5,7 @@ const State = require('vigour-state')
 const Element = require('../lib/element')
 const render = require('../lib/render')
 // -------------------------
-require('./style.less')
+require('./style.css')
 // -------------------------
 const raf = window.requestAnimationFrame
 const isNumber = require('vigour-util/is/number')

9b741bd96887311765af6ea6f93ebc0597133709
Jim de Beer <jim@vigour.io>
2016-04-29 14:05:48 +0200

    fixed defaultSubscription

--- example/root/index.js
+++ example/root/index.js
@@ -6,16 +6,12 @@ const Element = require('../../lib/element')
 const render = require('../../lib/render')
 const s = new State({
   key: 'STATE',
-  // a: {
-  //   b: 'its a.b'
-  // },
   c: {
     val: 'bla',
     d: {
       e: 'e'
     }
   }
-  // b: 'its root.b'
 })
 
 const app = new Element({
@@ -26,23 +22,23 @@ const app = new Element({
     b: {
       text: { $: '$root.b' }, // fucker fires WRONG!
       c: {
-        text: { $: '$root.c' }
+        text: { $: '$root.c', $prepend: 'a.b.c: ' }
       }
     }
+  },
+  holder: {
+    // $: '$any.collection'
+    $any: true, // unify and make easier
+    $: 'collection',
+    Child: {
+      text: { $: '$root.c' }
+    }
   }
-  // holder: {
-  //   // $: '$any.collection'
-  //   $any: true, // unify and make easier
-  //   $: 'collection',
-  //   Child: {
-  //     text: { $: '$root.c' }
-  //   }
-  // }
 })
 
 // const app = new Element({ text: 'hello' })
-document.body.appendChild(render(app, s, 'dom', function (state, type, stamp, subs, tree, ptree, fromTree) {
-  console.error('FIRE:', state.path(), type, stamp, tree, fromTree)
+document.body.appendChild(render(app, s, 'dom', function (state, type, stamp, subs, tree) {
+  console.error('FIRE:', state.path(), type, stamp, tree)
 }))
 
 console.log('\n\nCREATE ROOT:')
@@ -66,3 +62,7 @@ s.set({
 console.log('\n\n\nupdate YUZ')
 // shit shit shit <--- root in colleciton wrong!!!!
 s.c.set('yuz')
+
+console.log('\n\n\nupdate BLURF')
+
+s.c.set('blurf')

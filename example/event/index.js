bb42694dcc3c543148762fbda78360e07dd3e0de
Youri Daamen <me@youz.io>
2016-05-02 21:00:55 +0200

    start of events

--- example/event/index.js
+++ example/event/index.js
@@ -1,19 +1,36 @@
 'use strict'
 require('../style.css')
+const s = require('vigour-state/s')
 
 const render = require('../../lib/render')
 const elem = {
   key: 'app',
   holder: {
-    text: 'event test!',
-    on: {
-      mousedown (event) {
-        console.error('mousedown!', event)
+    $: 'collection',
+    $any: true,
+    Child: {
+      big: {
+        text: {
+          $: 'title'
+        },
+        on: {
+          drag (data) {
+            console.log('drag!!!')
+            data.state.title.set(data.state.title.val + '.')
+          }
+        }
       }
     }
   }
 }
 
-console.log('----render----')
-document.body.appendChild(render(elem))
+const data = {}
+let n = 1
 
+data.collection = {}
+
+while (n--) data.collection[n] = {title: n}
+
+const state = s(data)
+
+document.body.appendChild(render(elem, state))

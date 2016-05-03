ab27b653c80408ad0d0253127344622479be4f4a
Jim de Beer <jim@vigour.io>
2016-05-03 18:56:08 +0200

    remove pnode guard (nested removals need to go away anyway)

--- example/context/index.js
+++ example/context/index.js
@@ -22,49 +22,49 @@ document.body.appendChild(render({
       }
     }
   },
-  // holder: {
-  //   rowA: { type: 'item' },
-  //   rowB: { type: 'item' }
-  // },
-  holder2: {
-    class: 'holder',
-    rowA: {
-      class: 'complex-item',
-      symbol: {},
-      title: { text: 'no context' },
-      first: { $: 'a', class: 'nested', b: { c: { text: { $: 'first' } } } },
-      second: { $: 'b', class: 'nested', b: { c: { text: { $: 'second' } } } }
-    },
-    rowB: {
-      class: 'complex-item',
-      symbol: {},
-      title: { text: 'no context' },
-      subtitle: { text: 'path subscription' },
-      first: { class: 'basic-item', $: 'a.first', text: 'first' },
-      second: { class: 'basic-item', $: 'b.second', text: 'second' }
-    },
-    rowC: {
-      class: 'complex-item',
-      symbol: {},
-      title: { text: 'no context' },
-      subtitle: { text: 'mixed subscription' },
-      first: { class: 'basic-item', $: 'a', text: 'first' },
-      second: { class: 'basic-item', $: 'b.second', text: 'second' }
-    }
-    // rowD: {
-    //   $: 'rootspawner',
-    //   class: 'complex-item',
-    //   symbol: {},
-    //   title: { text: 'no context' },
-    //   subtitle: { text: 'root subscription' },
-    //   // now this does nto fire... on remove
-    //   // and fires one to may after the remove for b
-    //   first: { class: 'basic-item', $: '$root.a.first', text: 'first' },
-    //   second: { class: 'basic-item', $: '$root.b.second', text: 'second' }
-    // }
+  holder: {
+    rowA: { type: 'item' },
+    rowB: { type: 'item' }
   }
+  // holder2: {
+  //   class: 'holder',
+  //   rowA: {
+  //     class: 'complex-item',
+  //     symbol: {},
+  //     title: { text: 'no context' },
+  //     first: { $: 'a', class: 'nested', b: { c: { text: { $: 'first' } } } },
+  //     second: { $: 'b', class: 'nested', b: { c: { text: { $: 'second' } } } }
+  //   },
+  //   rowB: {
+  //     class: 'complex-item',
+  //     symbol: {},
+  //     title: { text: 'no context' },
+  //     subtitle: { text: 'path subscription' },
+  //     first: { class: 'basic-item', $: 'a.first', text: 'first' },
+  //     second: { class: 'basic-item', $: 'b.second', text: 'second' }
+  //   },
+  //   rowC: {
+  //     class: 'complex-item',
+  //     symbol: {},
+  //     title: { text: 'no context' },
+  //     subtitle: { text: 'mixed subscription' },
+  //     first: { class: 'basic-item', $: 'a', text: 'first' },
+  //     second: { class: 'basic-item', $: 'b.second', text: 'second' }
+  //   }
+  //   // rowD: {
+  //   //   $: 'rootspawner',
+  //   //   class: 'complex-item',
+  //   //   symbol: {},
+  //   //   title: { text: 'no context' },
+  //   //   subtitle: { text: 'root subscription' },
+  //   //   // now this does nto fire... on remove
+  //   //   // and fires one to may after the remove for b
+  //   //   first: { class: 'basic-item', $: '$root.a.first', text: 'first' },
+  //   //   second: { class: 'basic-item', $: '$root.b.second', text: 'second' }
+  //   // }
+  // }
 }, state, (state, type, stamp, tree, subs, sType) => {
-  console.log('\nFIRE:', state.path().join('/'), ' - ', type, ' - ', sType || 'normal')
+  console.log('%cFIRE', 'color: white;background-color: #333; padding: 2px;', state.path().join('/'), ' - ', type, ' - ', sType || 'normal', '\n\n')
 }))
 
 state.title.set('third')
@@ -74,4 +74,4 @@ state.set({ a: { first: 'first' } })
 
 console.error('\nREMOVE!')
 // context removal is still broken
-// state.set({ a: null })
+state.set({ a: null })

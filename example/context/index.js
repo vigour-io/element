d4055f603fc7f5e0fa2ba735913a56925ce03dda
Jim de Beer <jim@vigour.io>
2016-05-04 15:32:54 +0200

    index

--- example/context/index.js
+++ example/context/index.js
@@ -5,16 +5,40 @@ const s = require('vigour-state/s')
 const state = s({ title: 'dynamic text' }, false)
 
 document.body.appendChild(render({
-  key: 'app',
-  text: 'context',
   components: {
-    item: {
+    basic: {
+      class: 'basic-item',
+      Child: { class: 'basic-item' },
+      first: { text: { $: 'first.text' } },
+      second: { text: 'static-second' }
+    },
+    complex: {
       class: 'complex-item',
-      first: { $: 'first', class: 'nested', b: { c: { text: { $: 'text' } } } },
-      second: { $: 'second', class: 'nested', b: { c: { text: { $: 'text' } } } },
+      first: {
+        $: 'first', class: 'nested',
+        b: {
+          c: {
+            text: { $: 'text' },
+            field: { text: 'static-second' }
+          }
+        }
+      },
+      second: {
+        $: 'second',
+        class: 'nested',
+        b: {
+          c: {
+            text: { $: 'text' }
+          }
+        }
+      },
       title: { text: 'context' },
       subtitle: { text: 'static & state order' },
-      nested: { b: { c: { text: { $: 'title' } } } },
+      nested: {
+        b: {
+          c: { text: { $: 'title' } }
+        }
+      },
       symbol: {},
       symbol2: {
         class: 'symbol',
@@ -22,65 +46,71 @@ document.body.appendChild(render({
       }
     }
   },
+  key: 'app',
+  text: 'context',
+  Child: { class: 'holder' },
   holder: {
-    rowA: { type: 'item' },
-    rowB: { type: 'item' }
-  },
-  updateText: {
-    class: 'basic-item',
-    text: 'update all text',
-    on: {
-      click (data) {
-        function updateText (state) {
-          if (state.val) {
-            const split = state.val.split(' ')
-            state.set(split[0] + ' ' + Math.round(Math.random() * 9999))
-          }
-          state.each(updateText)
-        }
-        updateText(state)
-      }
-    }
-  },
-  toggle: {
-    class: 'basic-item',
-    text: {
-      $: 'first.text',
-      $transform (val) {
-        return !val ? 'add first' : 'remove ' + val
-      }
-    },
-    on: {
-      click (data) {
-        state.set({ first: state.first ? null : { text: 'first' } })
-      }
-    }
+    a: { type: 'basic' },
+    b: { type: 'basic' }
   },
   holder2: {
-    class: 'holder',
-    rowA: {
-      class: 'complex-item',
-      symbol: {},
-      title: { text: 'no context' },
-      first: { $: 'first', class: 'nested', b: { c: { text: { $: 'text' } } } },
-      second: { $: 'second', class: 'nested', b: { c: { text: { $: 'text' } } } }
-    },
-    rowB: {
-      class: 'complex-item',
-      symbol: {},
-      title: { text: 'no context' },
-      subtitle: { text: 'path subscription' },
-      first: { class: 'basic-item', $: 'first.text', text: 'first' },
-      second: { class: 'basic-item', $: 'second.text', text: 'second' }
-    },
-    rowC: {
-      class: 'complex-item',
-      symbol: {},
-      title: { text: 'no context' },
-      subtitle: { text: 'mixed subscription' },
-      first: { class: 'basic-item', $: 'first', text: 'first' },
-      second: { class: 'basic-item', $: 'second.text', text: 'second' }
-    }
+    a: { type: 'complex' },
+    b: { type: 'complex' }
+  },
+  // updateText: {
+  //   class: 'basic-item',
+  //   text: 'update all text',
+  //   on: {
+  //     click (data) {
+  //       function updateText (state) {
+  //         if (state.val) {
+  //           const split = state.val.split(' ')
+  //           state.set(split[0] + ' ' + Math.round(Math.random() * 9999))
+  //         }
+  //         state.each(updateText)
+  //       }
+  //       updateText(state)
+  //     }
+  //   }
+  // },
+  // toggle: {
+  //   class: 'basic-item',
+  //   text: {
+  //     $: 'first.text',
+  //     $transform (val) {
+  //       return !val ? 'add first' : 'remove ' + val
+  //     }
+  //   },
+  //   on: {
+  //     click (data) {
+  //       state.set({ first: state.first ? null : { text: 'first' } })
+  //     }
+  //   }
+  // },
+  // holder2: {
+  //   rowA: {
+  //     class: 'complex-item',
+  //     symbol: {},
+  //     title: { text: 'no context' },
+  //     first: { $: 'first', class: 'nested', b: { c: { text: { $: 'text' } } } },
+  //     second: { $: 'second', class: 'nested', b: { c: { text: { $: 'text' } } } }
+  //   },
+  //   rowB: {
+  //     class: 'complex-item',
+  //     symbol: {},
+  //     title: { text: 'no context' },
+  //     subtitle: { text: 'path subscription' },
+  //     first: { class: 'basic-item', $: 'first.text', text: 'first' },
+  //     second: { class: 'basic-item', $: 'second.text', text: 'second' }
+  //   },
+  //   rowC: {
+  //     class: 'complex-item',
+  //     symbol: {},
+  //     title: { text: 'no context' },
+  //     subtitle: { text: 'mixed subscription' },
+  //     first: { class: 'basic-item', $: 'first', text: 'first' },
+  //     second: { class: 'basic-item', $: 'second.text', text: 'second' }
+  //   }
     // rowD: {
     //   $: 'rootspawner',
     //   class: 'complex-item',
@@ -92,7 +122,7 @@ document.body.appendChild(render({
     //   first: { class: 'basic-item', $: '$root.a.first', text: 'first' },
     //   second: { class: 'basic-item', $: '$root.b.second', text: 'second' }
     // }
-  }
+  // }
 }, state, (state, type, stamp, tree, subs, sType) => {
   console.log('%cFIRE', 'color: white;background-color: #333; padding: 2px;', state.path().join('/'), ' - ', type, ' - ', sType || 'normal', '\n\n')
 }))

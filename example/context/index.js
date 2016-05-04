68b1f136f312784f37131228d56cf18d2896c28a
Jim de Beer <jim@vigour.io>
2016-05-04 16:17:22 +0200

    added text property type

--- example/context/index.js
+++ example/context/index.js
@@ -9,17 +9,22 @@ document.body.appendChild(render({
     basic: {
       class: 'basic-item',
       Child: { class: 'basic-item' },
-      first: { text: { $: 'first.text' } },
+      first: {
+        $: 'first',
+        text: { $: 'text' }
+      },
       second: { text: 'static-second' }
     },
     complex: {
       class: 'complex-item',
       first: {
-        $: 'first', class: 'nested',
+        $: 'first',
+        class: 'nested',
         b: {
           c: {
-            text: { $: 'text' },
-            field: { text: 'static-second' }
+            Child: { class: 'basic-item' },
+            a: { text: { $: 'text' } },
+            b: { text: 'static-second' }
           }
         }
       },
@@ -53,26 +58,26 @@ document.body.appendChild(render({
     { type: 'basic' },
     { type: 'basic' }
   ],
-  holder2: [
-    { type: 'complex' },
-    { type: 'complex' }
-  ],
-  updateText: {
-    class: 'basic-item',
-    text: 'update all text',
-    on: {
-      click (data) {
-        function updateText (state) {
-          if (state.val) {
-            const split = state.val.split(' ')
-            state.set(split[0] + ' ' + Math.round(Math.random() * 9999))
-          }
-          state.each(updateText)
-        }
-        updateText(state)
-      }
-    }
-  },
+  // holder2: [
+  //   { type: 'complex' },
+  //   { type: 'complex' }
+  // ],
+  // updateText: {
+  //   class: 'basic-item',
+  //   text: 'update all text',
+  //   on: {
+  //     click (data) {
+  //       function updateText (state) {
+  //         if (state.val) {
+  //           const split = state.val.split(' ')
+  //           state.set(split[0] + ' ' + Math.round(Math.random() * 9999))
+  //         }
+  //         state.each(updateText)
+  //       }
+  //       updateText(state)
+  //     }
+  //   }
+  // },
   toggle: {
     class: 'basic-item',
     text: {
@@ -86,45 +91,32 @@ document.body.appendChild(render({
         state.set({ first: state.first ? null : { text: 'first' } })
       }
     }
-  },
-  holder3: [
-    {
-      class: 'complex-item',
-      symbol: {},
-      title: { text: 'no context' },
-      first: { $: 'first', class: 'nested', b: { c: { text: { $: 'text' } } } },
-      second: { $: 'second', class: 'nested', b: { c: { text: { $: 'text' } } } }
-    },
-    {
-      class: 'complex-item',
-      symbol: {},
-      title: { text: 'no context' },
-      subtitle: { text: 'path subscription' },
-      first: { class: 'basic-item', $: 'first.text', text: 'first' },
-      second: { class: 'basic-item', $: 'second.text', text: 'second' }
-    },
-    {
-      class: 'complex-item',
-      symbol: {},
-      title: { text: 'no context' },
-      subtitle: { text: 'mixed subscription' },
-      first: { class: 'basic-item', $: 'first', text: 'first' },
-      second: { class: 'basic-item', $: 'second.text', text: 'second' }
-    }
-    // HAS TO BE DONE BY TMRW!
-
-    // {
-    //   $: 'rootspawner',
-    //   class: 'complex-item',
-    //   symbol: {},
-    //   title: { text: 'no context' },
-    //   subtitle: { text: 'root subscription' },
-    //   // now this does nto fire... on remove
-    //   // and fires one to may after the remove for b
-    //   first: { class: 'basic-item', $: '$root.a.first', text: 'first' },
-    //   second: { class: 'basic-item', $: '$root.b.second', text: 'second' }
-    // }
-  ]
+  }
+  // holder3: [
+  //   {
+  //     class: 'complex-item',
+  //     symbol: {},
+  //     title: { text: 'no context' },
+  //     first: { $: 'first', class: 'nested', b: { c: { text: { $: 'text' } } } },
+  //     second: { $: 'second', class: 'nested', b: { c: { text: { $: 'text' } } } }
+  //   },
+  //   {
+  //     class: 'complex-item',
+  //     symbol: {},
+  //     title: { text: 'no context' },
+  //     subtitle: { text: 'path subscription' },
+  //     first: { class: 'basic-item', $: 'first.text', text: 'first' },
+  //     second: { class: 'basic-item', $: 'second.text', text: 'second' }
+  //   },
+  //   {
+  //     class: 'complex-item',
+  //     symbol: {},
+  //     title: { text: 'no context' },
+  //     subtitle: { text: 'mixed subscription' },
+  //     first: { class: 'basic-item', $: 'first', text: 'first' },
+  //     second: { class: 'basic-item', $: 'second.text', text: 'second' }
+  //   }
+  // ]
 }, state, (state, type, stamp, tree, subs, sType) => {
   console.log('%cFIRE', 'color: white;background-color: #333; padding: 2px;', state.path().join('/'), ' - ', type, ' - ', sType || 'normal', '\n\n')
 }))
@@ -133,3 +125,17 @@ state.title.set('third')
 state.set({ second: { text: 'second' } })
 state.set({ first: { text: 'first' } })
 // state.set({ rootspawner: {} })
+
+// HAS TO BE DONE BY TMRW!
+// ROOT
+// {
+//   $: 'rootspawner',
+//   class: 'complex-item',
+//   symbol: {},
+//   title: { text: 'no context' },
+//   subtitle: { text: 'root subscription' },
+//   // now this does nto fire... on remove
+//   // and fires one to may after the remove for b
+//   first: { class: 'basic-item', $: '$root.a.first', text: 'first' },
+//   second: { class: 'basic-item', $: '$root.b.second', text: 'second' }
+// }

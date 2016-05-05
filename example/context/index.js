cc0c5c6188653f736d77fb140c14714d22f8d1f3
Jim de Beer <jim@vigour.io>
2016-05-05 10:23:58 +0200

    props usecase

--- example/context/index.js
+++ example/context/index.js
@@ -7,10 +7,31 @@ const state = s({ greeting: 'hello' })
 document.body.appendChild(render({
   text: 'context',
   key: 'app',
-  Child: { class: 'holder' }
+  Child: { class: 'holder' },
+  propsElem: {
+    title: { text: 'props' },
+    props: {
+      components: {
+        greeting: {
+          $: 'greeting',
+          $transform (val) {
+            return (val !== this && val !== true && val) || 'hahaha'
+          }
+        }
+      },
+      properties: {
+        // when making into context goes wrong (makes sense)
+        hello: { type: 'greeting' }
+      },
+      hello: true,
+      bye: { type: 'greeting', $: false }
+    }
+  }
 }, state, (state, type, stamp, tree, subs, sType) => {
   console.log('%cFIRE', 'color: white;background-color: #333; padding: 2px;', state.path().join('/'), ' - ', type, ' - ', sType || 'normal', '\n\n')
 }))
+
+console.log(document.body.children[2].children[0])
 
 // props
 // then group

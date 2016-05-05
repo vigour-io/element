74e20f41b0be594d4998bccea01172b4a642174c
Jim de Beer <jim@vigour.io>
2016-05-05 10:51:44 +0200

    props fix for static, fixing props for context

--- example/context/index.js
+++ example/context/index.js
@@ -2,37 +2,53 @@
 require('../style.css')
 const render = require('../../lib/render')
 const s = require('vigour-state/s')
-const state = s({ greeting: 'hello' })
+const state = s({ greeting: 'hello', field: 'field' })
 
 document.body.appendChild(render({
   text: 'context',
   key: 'app',
   Child: { class: 'holder' },
-  propsElem: {
-    title: { text: 'props' },
-    props: {
-      components: {
-        greeting: {
-          $: 'greeting',
-          $transform (val) {
-            return (val !== this && val !== true && val) || 'hahaha'
+  components: {
+    propsElem: {
+      props: {
+        components: {
+          greeting: {
+            $: 'greeting',
+            $transform (val) {
+              return (val !== this && val !== true && val) || 'hahaha'
+            }
           }
-        }
-      },
-      properties: {
-        // when making into context goes wrong (makes sense)
-        hello: { type: 'greeting' }
-      },
-      hello: true,
-      bye: { type: 'greeting', $: false }
+        },
+        properties: {
+          // when making into context goes wrong (makes sense)
+          greeting: { type: 'greeting' }
+        },
+        greeting: true,
+        otherGreeting: { type: 'greeting' },
+        staticGreeting: { type: 'greeting', $: false, val: 'gutten morgen' }
+      }
     }
+  },
+  propsholder: {
+    title: { text: 'props' },
+    a: { type: 'propsElem' }
+    // components: {
+    //   a: { $: 'greeting' }
+    // },
+    // a: { type: 'a' },
+    // b: { type: 'a', $: 'field' },
+    // c: { type: 'a', $: false }
   }
 }, state, (state, type, stamp, tree, subs, sType) => {
+  // pass app, rState, rTree
   console.log('%cFIRE', 'color: white;background-color: #333; padding: 2px;', state.path().join('/'), ' - ', type, ' - ', sType || 'normal', '\n\n')
 }))
 
-console.log(document.body.children[2].children[0])
-
+console.log(document.body.children[2].children[0].children[1])
+state.greeting.set('bye')
+console.log(document.body.children[2].children[0].children[1])
+// state.field.remove()
+// console.log(document.body.children[2].children[0])
 // props
 // then group
 // then style

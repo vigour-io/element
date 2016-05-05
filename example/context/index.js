25720cafe008049c660abf1ae1845933ac2d6fb0
Jim de Beer <jim@vigour.io>
2016-05-05 11:39:43 +0200

    added breaking usecase

--- example/context/index.js
+++ example/context/index.js
@@ -2,13 +2,41 @@
 require('../style.css')
 const render = require('../../lib/render')
 const s = require('vigour-state/s')
-const state = s({ greeting: 'hello', field: 'field' })
+const state = s({
+  greeting: 'hello',
+  field: 'field',
+  a: {
+    b: 'a/b'
+  },
+  c: {
+    b: 'c/b'
+  },
+  collection: [
+    { b: '0/b' },
+    { b: '1/b' },
+    { b: '2/b' }
+  ]
+})
 
 document.body.appendChild(render({
   text: 'context',
   key: 'app',
   Child: { class: 'holder' },
   components: {
+    t: {
+      type: 'text',
+      $: 'b',
+      $transform (val) { return ` ${val} ` }
+    },
+    other: {
+      class: 'basic-item',
+      $: 'a',
+      Child: { type: 't' },
+      t: {},
+      t2: {},
+      t3: {},
+      t4: { $: false, val: 'static' }
+    },
     propsElem: {
       props: {
         components: {
@@ -27,17 +55,36 @@ document.body.appendChild(render({
         otherGreeting: { type: 'greeting' },
         staticGreeting: { type: 'greeting', $: false, val: 'gutten morgen' }
       }
+    },
+    collection: {
+      class: 'complex-item',
+      title: { text: 'collection' },
+      $any: true,
+      $: 'collection',
+      Child: { text: { $: 'b' } } // this is def broken
+      // Child: { type: 'other', $: false }
     }
   },
-  propsholder: {
-    title: { text: 'props' },
-    a: { type: 'propsElem' }
-    // components: {
-    //   a: { $: 'greeting' }
-    // },
-    // a: { type: 'a' },
-    // b: { type: 'a', $: 'field' },
-    // c: { type: 'a', $: false }
+  // propsholder: {
+  //   title: { text: 'props' }
+  //   // a: { type: 'propsElem' }
+  //   // components: {
+  //   //   a: { $: 'greeting' }
+  //   // },
+  //   // a: { type: 'a' },
+  //   // b: { type: 'a', $: 'field' },
+  //   // c: { type: 'a', $: false }
+  // },
+  // elems: {
+  //   title: { text: 'elements' },
+  //   other2: { type: 'other', $: 'c' },
+  //   other: { type: 'other' }
+  // },
+  collections: {
+    title: { text: 'collections' },
+    collection: { type: 'collection' } // this is def wrong
+    // collection2: { type: 'collection' },
+    // collection3: { type: 'collection' }
   }
 }, state, (state, type, stamp, tree, subs, sType) => {
   // pass app, rState, rTree
@@ -47,6 +94,8 @@ document.body.appendChild(render({
 console.log(document.body.children[2].children[0].children[1])
 state.greeting.set('bye')
 console.log(document.body.children[2].children[0].children[1])
+// state.c.b.set('c-b')
+
 // state.field.remove()
 // console.log(document.body.children[2].children[0])
 // props

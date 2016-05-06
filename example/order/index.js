47046c90337a01aae6aa632a0a2a25d2262dc99c
Jim de Beer <jim@vigour.io>
2016-05-06 14:41:07 +0200

    fixes

--- example/order/index.js
+++ example/order/index.js
@@ -13,45 +13,60 @@ const app = {
   Child: { class: 'holder' },
   // properties: { texts: { type: 'texts' } },
   // texts: [ '-', { $: 'first.text' }, '-' ],
-  // updateText: {
-  //   class: 'basic-item',
-  //   text: 'update all text',
-  //   on: {
-  //     click (data) {
-  //       function updateText (state) {
-  //         if (state.val) {
-  //           const split = state.val.split(' ')
-  //           state.set(split[0] + ' ' + Math.round(Math.random() * 9999))
-  //         }
-  //         state.each(updateText)
-  //       }
-  //       updateText(state)
-  //     }
-  //   }
-  // },
-  // toggle: {
-  //   class: 'basic-item',
-  //   text: {
-  //     // completely wrong allready
-  //     $: 'first.text',
-  //     $transform (val) {
-  //       return !val ? 'add first' : 'remove ' + val
-  //     }
-  //   },
-  //   on: {
-  //     click (data) {
-  //       state.set({ first: state.first ? null : { text: 'first' } })
-  //     }
-  //   }
-  // },
-  nocontext: [
-    {
-      class: 'complex-item',
-      symbol: {},
-      title: { text: 'no context' },
-      first: { $: 'first', class: 'nested', b: { c: { text: { $: 'text' } } } },
-      // second: { $: 'second', class: 'nested', b: { c: { text: { $: 'text' } } } }
+  updateText: {
+    class: 'basic-item',
+    text: 'update all text',
+    on: {
+      click (data) {
+        function updateText (state) {
+          if (state.val) {
+            const split = state.val.split(' ')
+            state.set(split[0] + ' ' + Math.round(Math.random() * 9999))
+          }
+          state.each(updateText)
+        }
+        updateText(state)
+      }
+    }
+  },
+  toggle: {
+    class: 'basic-item',
+    text: {
+      // completely wrong allready
+      $: 'first.text',
+      $transform (val) {
+        return !val ? 'add first' : 'remove ' + val
+      }
     },
+    on: {
+      click (data) {
+        state.set({ first: state.first ? null : { text: 'first' } })
+      }
+    }
+  },
+  toggle2: {
+    class: 'basic-item',
+    text: {
+      // completely wrong allready
+      $: 'second.text',
+      $transform (val) {
+        return !val ? 'add second' : 'remove ' + val
+      }
+    },
+    on: {
+      click (data) {
+        state.set({ second: state.second ? null : { text: 'second' } })
+      }
+    }
+  },
+  nocontext: [
+    // {
+    //   class: 'complex-item',
+    //   symbol: {},
+    //   title: { text: 'no context' },
+    //   first: { $: 'first', class: 'nested', b: { c: { text: { $: 'text' } } } },
+    //   // second: { $: 'second', class: 'nested', b: { c: { text: { $: 'text' } } } }
+    // },
     // {
     //   class: 'complex-item',
     //   symbol: {},
@@ -109,7 +124,7 @@ console.log('TOP TA')
 logger(subs._.ta)
 
 console.log('TOP FIRST TA')
-logger(subs.first._.ta)
+// logger(subs.first._.ta)
 
 state.set({ second: { text: 'second' } })
 state.set({ first: { text: 'first' } })
